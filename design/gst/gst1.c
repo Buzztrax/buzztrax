800a835303137b93d5807205a5d5bbc1b0bdbf96
Stefan Sauer <ensonic@users.sf.net>
2004-02-10 14:30:20 +0000

    A: first gstreamer example

---
+++ design/gst/gst1.c
@@ -0,0 +1,14 @@
+/** $Id: gst1.c,v 1.1 2004-02-10 14:29:48 ensonic Exp $
+ */
+ 
+#include <stdio.h>
+#include <gst/gst.h>
+
+int main(int argc, char **argv) {
+	GstElement *element;
+	
+	gst_init(&argc, &argv);
+
+ 
+	return(0);
+}
