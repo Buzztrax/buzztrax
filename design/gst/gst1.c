d806f7941071ef004c12e880a3af6a90d1c0781a
Thomas Wabner <waffel@users.sf.net>
2004-02-11 12:44:24 +0000

    pipeline works now. Dynamic parameter value added

--- design/gst/gst1.c
+++ design/gst/gst1.c
@@ -1,44 +1,74 @@
-/** $Id: gst1.c,v 1.2 2004-02-11 11:07:27 waffel Exp $
+/** $Id: gst1.c,v 1.3 2004-02-11 12:44:24 waffel Exp $
  */
  
 #include <stdio.h>
 #include <gst/gst.h>
+#include <gst/control/control.h>
 
 int main(int argc, char **argv) {
-	GstElement *audiosink, *sinesrc, *pipeline;
-  int counter=0;
-  GstElementStateReturn ret;
-	
+	GstElement *audiosink, *audiosrc, *pipeline, *audioconvert;
+  GstDParamManager *audiosrcParam;
+  GstDParam *volume;
+  gfloat set_to_value;
+  GValue *set_val;
+  
 	gst_init(&argc, &argv);
+  gst_control_init(&argc,&argv);
+  
+  if (argc < 3) {
+    printf("use: %s <src> <sink> <value>\nvalue should be between 0 ... 100\n",argv[0]);
+    exit(-1);
+  }
+  
   
   /* create a new pipeline to hold the elements */
   pipeline = gst_pipeline_new ("pipeline");
   
   /* and an audio sink */
-  audiosink = gst_element_factory_make ("esdsink", "play_audio");
-  if (audiosink == NULL) {
+  audiosink = gst_element_factory_make (argv[2], "play_audio");
+  if (audiosrc == NULL) {
     exit (-1);
   }
   
+  audioconvert = gst_element_factory_make ("audioconvert", "convert");
+  
   /* add an sine source */
-  sinesrc = gst_element_factory_make ("sinesrc", "sine-source");
-  if (sinesrc == NULL) {
-    exit (-2);
+  audiosrc = gst_element_factory_make (argv[1], "audio-source");
+  if (audiosrc == NULL) {
+    exit (-1);
+  }
+  
+  set_to_value = atof(argv[3]);
+  set_to_value = set_to_value/100;
+  /* getting param manager */
+  audiosrcParam = gst_dpman_get_manager (audiosrc);
+  /* setting param mode. Only synchronized currently supported */
+  gst_dpman_set_mode(audiosrcParam, "synchronous");
+  volume = gst_dparam_new(G_TYPE_FLOAT);
+  if (gst_dpman_attach_dparam (audiosrcParam, "volume", volume)){
+    /* the dparam was successfully attached */
+    set_val = g_new0(GValue,1);
+    g_value_init(set_val, G_TYPE_FLOAT);
+    g_value_set_float(set_val, set_to_value);
+    g_object_set_property(G_OBJECT(volume), "value_float", set_val);
   }
   
   /* add objects to the main pipeline */
-  gst_bin_add_many (GST_BIN (pipeline), sinesrc, audiosink, NULL);
+  gst_bin_add_many (GST_BIN (pipeline), audiosrc, audioconvert, audiosink, NULL);
+  
+   /* link src to sink */
+  gst_element_link_many (audiosrc, audioconvert, audiosink, NULL);
   
   /* start playing */
-  ret = gst_element_set_state (pipeline, GST_STATE_READY);
-  printf("%d\n",ret);
-  
-  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
-  printf("%d\n",ret);
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
   while (gst_bin_iterate (GST_BIN (pipeline))){
-    counter++;
-    printf("%d\n",counter);
+    set_to_value=set_to_value-0.01;
+    g_value_set_float(set_val, set_to_value);
+    g_object_set_property(G_OBJECT(volume), "value_float", set_val);
+    if (set_to_value < 0.1) {
+      break;
+    }
   }
 
   /* stop the pipeline */
