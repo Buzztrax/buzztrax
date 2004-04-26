460c2692916fba65660050dd1fae2621451943c2
Stefan Sauer <ensonic@users.sf.net>
2004-04-26 14:43:29 +0000

    C: we have no a timeline cursor object C: therewith we support pattern with different length

--- src/lib/core/libbtcore/pattern.h
+++ src/lib/core/libbtcore/pattern.h
@@ -1,4 +1,4 @@
-/** $Id: pattern.h,v 1.3 2004-04-23 12:38:18 ensonic Exp $
+/** $Id: pattern.h,v 1.4 2004-04-26 14:43:29 ensonic Exp $
  * patterns/sequence
  *
  */
@@ -10,10 +10,16 @@ typedef struct _BTPattern BTPattern;
 typedef BTPattern *BTPatternPtr;
 struct _BTPattern {
 	BtMachinePtr machine;
-	GValue **parameters;
 	// GValue parameters[tick_len][machine->dparams_count]
 	// with NULL as end-marker in tick_len
-	GValue **parameter;
+	GValue **parameters;	// every elemnt is a pointer to GValue or NULL
+};
+
+typedef struct _BTTimeLine BTTimeLine;
+typedef BTTimeLine *BTTimeLinePtr;
+struct _BTTimeLine {
+	BTPatternPtr *tracks;	// PatternPtr or NULL
+	GValue ***parameter;		// array with positions in Pattern->parameters or NULL
 };
 
 #endif /* BT_PATTERN_H */
