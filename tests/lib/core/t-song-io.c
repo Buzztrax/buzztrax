23be7f9ba187b06ec8ef982752b60d07508875a7
Thomas Wabner <waffel@users.sf.net>
2004-09-16 16:24:39 +0000

    [feature] added more tests

--- tests/lib/core/t-song-io.c
+++ tests/lib/core/t-song-io.c
@@ -1,4 +1,4 @@
-/** $Id: t-song-io.c,v 1.2 2004-08-24 14:26:59 ensonic Exp $
+/** $Id: t-song-io.c,v 1.3 2004-09-16 16:24:39 waffel Exp $
 **/
 
 #include "t-song.h"
@@ -15,6 +15,7 @@ static void test_teardown(void) {
 
 //-- tests
 
+// try to create a songIO object with NULL pointer
 START_TEST(test_btsong_io_obj1) {
 	BtSongIO *songIO;
 	
@@ -23,10 +24,30 @@ START_TEST(test_btsong_io_obj1) {
 }
 END_TEST
 
+// try to create a songIO object with empty string
+START_TEST(test_btsong_io_obj2) {
+	BtSongIO *songIO;
+
+	songIO=bt_song_io_new("");
+	fail_unless(songIO==NULL, NULL);
+}
+END_TEST
+
+// try to create a songIO object with wrong song name
+START_TEST(test_btsong_io_obj3) {
+	BtSongIO *songIO;
+
+	songIO=bt_song_io_new("test");
+	fail_unless(songIO==NULL, NULL);
+}
+END_TEST
+
 TCase *bt_song_io_obj_tcase(void) {
   TCase *tc = tcase_create("bt_song_io case");
 
   tcase_add_test(tc,test_btsong_io_obj1);
+	tcase_add_test(tc,test_btsong_io_obj2);
+	tcase_add_test(tc,test_btsong_io_obj3);
   tcase_add_unchecked_fixture(tc, test_setup, test_teardown);
   return(tc);
 }
