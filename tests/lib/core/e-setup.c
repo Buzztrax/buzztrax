6e6a8d5fa9f4ee7bc9fe0ae74aa2f35ea3df59f0
Thomas Wabner <waffel@users.sf.net>
2004-10-04 16:09:47 +0000

    added more docu and another example

--- tests/lib/core/e-setup.c
+++ tests/lib/core/e-setup.c
@@ -1,4 +1,4 @@
-/** $Id: e-setup.c,v 1.2 2004-10-01 13:36:09 waffel Exp $
+/** $Id: e-setup.c,v 1.3 2004-10-04 16:09:47 waffel Exp $
 **/
 
 #include "t-core.h"
@@ -22,9 +22,10 @@ static void test_teardown(void) {
 //-- tests
 
 /**
-* Try to create a bt_machine_iterator with a real setup parameter.
-* This case demonstrates, how to create a machine iterator from the setup.
-* The returned pointer to the iterator should not be null.
+* In this test case we demonstrate how to create a setup and adding a machine
+* to the setup. 
+* Then we use the iterator to get the current added machine back and looking
+* if the machine was added nice.
 *
 */
 START_TEST(test_btsetup_obj1){
@@ -59,6 +60,7 @@ START_TEST(test_btsetup_obj1){
 	/* the pointer should be point to the gen1 machine */
 	ref_machine=bt_setup_machine_iterator_get_machine(iter_ptr);
 	fail_unless(ref_machine!=NULL, NULL);
+	fail_unless(ref_machine==BT_MACHINE(gen1), NULL);
 	fail_unless(BT_IS_SOURCE_MACHINE(ref_machine)==TRUE, NULL);
 	
 	/* the next element of the pointer should be null */
@@ -66,10 +68,74 @@ START_TEST(test_btsetup_obj1){
 }
 END_TEST
 
+/**
+* In this test case we demonstrate how to create a wire and adding the newly
+* created wire to the setup. After that, we try to get the same wire back, if
+* we give the source or dest machine.
+*/
+START_TEST(test_btsetup_obj2) {
+	BtSong *song=NULL;
+	BtSetup *setup=NULL;
+	GstElement *bin=NULL;
+	// machines
+	BtSourceMachine *source=NULL;
+	BtSinkMachine *sink=NULL;
+	// wire
+	BtWire *wire=NULL;
+	BtWire *ref_wire=NULL;
+	
+	gpointer *iter_ptr;
+	
+	GST_INFO("--------------------------------------------------------------------------------");
+	
+	bin = gst_thread_new("thread");
+
+	song=bt_song_new(GST_BIN(bin));
+	setup=bt_setup_new(song);
+	fail_unless(setup!=NULL, NULL);
+	
+	/* try to craete generator1 with sinesrc */
+  source = bt_source_machine_new(song,"generator1","sinesrc",0);
+  fail_unless(source!=NULL, NULL);
+	
+	/* try to create sink machine with esd sink */
+	sink = bt_sink_machine_new(song,"sink1","esdsink");
+	fail_unless(sink!=NULL, NULL);
+	
+	/* try to create the wire */
+	wire = bt_wire_new(song, BT_MACHINE(source), BT_MACHINE(sink));
+	fail_unless(wire!=NULL, NULL);
+	
+	/* try to add the machines to the setup. We must do this. */
+	bt_setup_add_machine(setup, BT_MACHINE(source));
+	bt_setup_add_machine(setup, BT_MACHINE(sink));
+	
+	/* try to add the wire to the setup */
+	bt_setup_add_wire(setup, wire);
+	
+	/* try to get the current added wire by the source machine. In this case the 
+	source of the wire is our source machine.*/
+	ref_wire = bt_setup_get_wire_by_src_machine(setup, BT_MACHINE(source));
+	fail_unless(ref_wire!=NULL, NULL);
+	fail_unless(ref_wire==wire, NULL);
+	
+	/* setting the ref wire back to NULL for next check. */
+	ref_wire = NULL;
+	
+	/* try to get the current added wire by the dest machine. In this case the
+	destination of the wire is our sink machine. */
+	ref_wire = bt_setup_get_wire_by_dst_machine(setup, BT_MACHINE(sink));
+	fail_unless(ref_wire!=NULL, NULL);
+	fail_unless(ref_wire==wire, NULL);
+	
+}
+END_TEST
+
 TCase *bt_setup_example_tcase(void) {
   TCase *tc = tcase_create("bt_setup example case");
 
   tcase_add_test(tc,test_btsetup_obj1);
+	tcase_add_test(tc,test_btsetup_obj2);
   tcase_add_unchecked_fixture(tc, test_setup, test_teardown);
   return(tc);
 }
