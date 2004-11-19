c2b55c85fdfa9e46ce71cd80c8dd1afb456431fe
Thomas Wabner <waffel@users.sf.net>
2004-11-19 18:59:49 +0000

    [intern] changed some test behavior; [feature] added check unref to some objects

--- tests/lib/core/t-sequence.c
+++ tests/lib/core/t-sequence.c
@@ -1,4 +1,4 @@
-/** $Id: t-sequence.c,v 1.1 2004-11-08 18:00:42 waffel Exp $ 
+/** $Id: t-sequence.c,v 1.2 2004-11-19 18:59:49 waffel Exp $ 
 **/
 
 #include "t-core.h"
@@ -45,12 +45,76 @@ START_TEST(test_btsequence_obj2) {
   bt_application_new(app);
 
 	song=bt_song_new(app);
-	sequence=bt_sequence_new(song);
+	g_object_get(BT_SONG(song), "sequence", &sequence, NULL);
 	fail_unless(sequence!=NULL,NULL);
 	
 	check_init_error_trapp("bt_sequence_set_machine_by_track","BT_IS_MACHINE(machine)");
 	bt_sequence_set_machine_by_track(sequence,0,NULL);
 	fail_unless(check_has_error_trapped(), NULL);
+	
+	g_object_try_unref(G_OBJECT(sequence));
+	g_object_checked_unref(G_OBJECT(song));
+	g_object_checked_unref(app);
+}
+END_TEST
+
+/* try to set a new machine for the sequence with NULL for the sequence parameter */
+START_TEST(test_btsequence_obj3) {
+	BtApplication *app=NULL;
+	BtSong *song=NULL;
+	BtSourceMachine *machine=NULL;
+	
+	GST_INFO("--------------------------------------------------------------------------------");
+	
+  /* create a dummy app */
+	app=g_object_new(BT_TYPE_APPLICATION,NULL);
+  bt_application_new(app);
+  
+  /* create a new song */
+	song=bt_song_new(app);
+	
+	/* try to create a source machine */
+	machine=bt_source_machine_new(song,"id","sinesrc",1);
+	fail_unless(machine!=NULL, NULL);
+	
+	check_init_error_trapp("bt_sequence_set_machine_by_track","BT_IS_SEQUENCE(self)");
+	bt_sequence_set_machine_by_track(NULL,0,BT_MACHINE(machine));
+	fail_unless(check_has_error_trapped(), NULL);
+	
+	g_object_try_unref(G_OBJECT(machine));
+	g_object_checked_unref(G_OBJECT(song));
+	g_object_checked_unref(app);
+}
+END_TEST
+
+/* try to check if BtSequence returns a NULL pointer for get_timeline if there
+ is no timeline defined (loaded) */
+START_TEST(test_btsequence_obj4) {
+	BtApplication *app=NULL;
+	BtSong *song=NULL;
+	BtSequence *sequence=NULL;
+	BtTimeLine *timeline=NULL;
+	
+	GST_INFO("--------------------------------------------------------------------------------");
+	
+	/* create a dummy app */
+	app=g_object_new(BT_TYPE_APPLICATION,NULL);
+  bt_application_new(app);
+  
+  /* create a new song */
+	song=bt_song_new(app);
+	
+	/* create a new sequence */
+	g_object_get(BT_SONG(song),"sequence",&sequence,NULL);
+	fail_unless(sequence!=NULL,NULL);
+	
+	/* start check */
+	timeline=bt_sequence_get_timeline_by_time(sequence,0);
+	fail_unless(timeline==NULL,NULL);
+	
+	g_object_try_unref(G_OBJECT(sequence));
+		g_object_try_unref(G_OBJECT(song));
+	g_object_checked_unref(app);
 }
 END_TEST
 
@@ -59,6 +123,8 @@ TCase *bt_sequence_obj_tcase(void) {
 
   tcase_add_test(tc,test_btsequence_obj1);
 	tcase_add_test(tc,test_btsequence_obj2);
+	tcase_add_test(tc,test_btsequence_obj3);
+	tcase_add_test(tc,test_btsequence_obj4);
   tcase_add_unchecked_fixture(tc, test_setup, test_teardown);
   return(tc);
 }
