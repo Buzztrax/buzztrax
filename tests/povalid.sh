5afa0535738f25930c1e8f212b936596eebeb067
Stefan Sauer <ensonic@users.sf.net>
2005-06-10 10:14:04 +0000

    [intern] integrated povalid.sh check [intern] fixed problem reported by povalid.sh

--- tests/povalid.sh
+++ tests/povalid.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# $Id: povalid.sh,v 1.1 2005-06-10 09:28:15 ensonic Exp $
+# $Id: povalid.sh,v 1.2 2005-06-10 10:14:04 ensonic Exp $
 # test validity of po i18n files
 
-res=`cd po;intltool-update -m 2>&1 | wc -l;cd ..`
+#res=`cd ../po;intltool-update -m 2>&1 | wc -l;cd ..`
+
+res=`(cd ../po && rm -f ./missing && intltool-update 2>/dev/null -m && \
+if [ -f ./missing ]; then exit 1; else exit 0; fi)`
 
 exit $res
