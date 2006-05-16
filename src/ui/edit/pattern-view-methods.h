97d9ebb360fe4042b980e09ac795aeb1615b7dfa
Stefan Sauer <ensonic@users.sf.net>
2006-05-16 22:06:02 +0000

    * src/ui/edit/pattern-view-methods.h: Added missing file (thanks Tommi Sakari Uimonen)

---
+++ src/ui/edit/pattern-view-methods.h
@@ -0,0 +1,13 @@
+/* $Id: pattern-view-methods.h,v 1.1 2006-05-16 22:06:02 ensonic Exp $
+ * defines all public methods of the pattern view widget class
+ */
+
+#ifndef BT_PATTERN_VIEW_METHODS_H
+#define BT_PATTERN_VIEW_METHODS_H
+
+#include "pattern-view.h"
+#include "edit-application.h"
+
+extern BtPatternView *bt_pattern_view_new(const BtEditApplication *app);
+
+#endif // BT_PATTERN_VIEW_METHDOS_H
