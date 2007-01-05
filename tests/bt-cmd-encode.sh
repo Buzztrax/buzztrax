a9845979dfd06a4a7879e6e25d58d4390df91a5c
Stefan Sauer <ensonic@users.sf.net>
2007-01-05 20:47:27 +0000

    * tests/bt-cmd-encode.sh: * tests/bt-cmd-info.sh: also log test runs

--- tests/bt-cmd-encode.sh
+++ tests/bt-cmd-encode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: bt-cmd-encode.sh,v 1.7 2006-08-27 20:31:30 ensonic Exp $
+# $Id: bt-cmd-encode.sh,v 1.8 2007-01-05 20:47:27 ensonic Exp $
 # run bt-cmd --command=encode on all example and test for crashes
 
 source bt-cfg.sh
@@ -12,6 +12,6 @@ for song in $E_SONGS; do
   echo "testing $song"
   audio=`basename $song .xml`
   audio="$TESTRESULTDIR/$audio.ogg"
-  libtool --mode=execute ../src/ui/cmd/bt-cmd -q --command=encode --input-file=$song --output-file=$audio
+  GST_DEBUG_NO_COLOR=1 GST_DEBUG="*:2,bt-*:4" libtool --mode=execute ../src/ui/cmd/bt-cmd 2>/tmp/bt_cmd_encode.log -q --command=encode --input-file=$song --output-file=$audio
   if [ $? -ne 0 ]; then exit 1; fi
 done
--- tests/bt-cmd-info.sh
+++ tests/bt-cmd-info.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: bt-cmd-info.sh,v 1.5 2006-08-27 20:02:55 ensonic Exp $
+# $Id: bt-cmd-info.sh,v 1.6 2007-01-05 20:47:27 ensonic Exp $
 # run bt-cmd --command=info on all example and test for crashes
 
 source bt-cfg.sh
@@ -12,6 +12,6 @@ for song in $E_SONGS; do
   echo "testing $song"
   info=`basename $song .xml`
   info="$TESTRESULTDIR/$info.txt"
-  libtool --mode=execute ../src/ui/cmd/bt-cmd >$info --command=info --input-file=$song
+  GST_DEBUG_NO_COLOR=1 GST_DEBUG="*:2,bt-*:4" libtool --mode=execute ../src/ui/cmd/bt-cmd >$info 2>/tmp/bt_cmd_info.log --command=info --input-file=$song
   if [ $? -ne 0 ]; then exit 1; fi
 done
