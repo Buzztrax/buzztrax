89782b42aa3bfa724e2378f3157a3f47d384b2b5
Stefan Sauer <ensonic@users.sf.net>
2011-12-08 07:51:35 +0000

    * src/lib/core/libbuzztard-core/childproxy.h: 	  Whitespace.

--- src/lib/core/libbuzztard-core/childproxy.h
+++ src/lib/core/libbuzztard-core/childproxy.h
@@ -29,7 +29,7 @@ G_BEGIN_DECLS
 
 #define BT_TYPE_CHILD_PROXY               (bt_child_proxy_get_type ())
 #define BT_CHILD_PROXY(obj)               (G_TYPE_CHECK_INSTANCE_CAST ((obj), BT_TYPE_CHILD_PROXY, BtChildProxy))
-#define BT_IS_CHILD_PROXY(obj)	         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), BT_TYPE_CHILD_PROXY))
+#define BT_IS_CHILD_PROXY(obj)	          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), BT_TYPE_CHILD_PROXY))
 #define BT_CHILD_PROXY_GET_INTERFACE(obj) (G_TYPE_INSTANCE_GET_INTERFACE ((obj), BT_TYPE_CHILD_PROXY, BtChildProxyInterface))
 
 typedef struct _BtChildProxy BtChildProxy;    /* dummy object */
