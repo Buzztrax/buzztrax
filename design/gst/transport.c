a6c9683a36b834ab76900eca04c1186cbca025e7
Stefan Sauer <ensonic@users.sf.net>
2012-01-10 22:18:29 +0100

    examples: gstreamer transport control example

---
+++ design/gst/transport.c
@@ -0,0 +1,87 @@
+/* test transport state handling
+ *
+ * gcc -Wall -g `pkg-config gstreamer-0.10 --cflags --libs` transport.c -o transport
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <gst/gst.h>
+
+
+static void
+event_loop (GstElement * bin)
+{
+  GstBus *bus;
+  GstMessage *message = NULL;
+
+  bus = gst_element_get_bus (GST_ELEMENT (bin));
+
+  while (TRUE) {
+    message = gst_bus_poll (bus, GST_MESSAGE_ANY, -1);
+
+    g_assert (message != NULL);
+    
+    GST_DEBUG_OBJECT(GST_MESSAGE_SRC(message), "message %s received",
+        GST_MESSAGE_TYPE_NAME(message));
+
+    switch (message->type) {
+      case GST_MESSAGE_REQUEST_STATE: {
+        GstState state;
+        
+        gst_message_parse_request_state(message,&state);
+        GST_INFO_OBJECT (GST_MESSAGE_SRC(message),
+            "request state on the bin: %s",gst_element_state_get_name(state));
+        //if(GST_MESSAGE_SRC(message) == GST_OBJECT(bin)) {
+          gst_element_set_state (bin, state);
+        //}
+      } break;        
+      case GST_MESSAGE_STATE_CHANGED:
+        if(GST_MESSAGE_SRC(message) == GST_OBJECT(bin)) {
+          GstState oldstate,newstate,pending;
+
+          gst_message_parse_state_changed(message,&oldstate,&newstate,&pending);
+          GST_INFO("state change on the bin: %s -> %s",gst_element_state_get_name(oldstate),gst_element_state_get_name(newstate));
+        }
+        break;
+      case GST_MESSAGE_WARNING:
+      case GST_MESSAGE_ERROR: {
+        GError *gerror;
+        gchar *debug;
+
+        gst_message_parse_error (message, &gerror, &debug);
+        gst_object_default_error (GST_MESSAGE_SRC (message), gerror, debug);
+        gst_message_unref (message);
+        g_error_free (gerror);
+        g_free (debug);
+        return;
+      }
+      default:
+        gst_message_unref (message);
+        break;
+    }
+  }
+}
+
+
+gint 
+main (gint argc, gchar **argv)
+{
+  GstElement *bin;
+  /* elements used in pipeline */
+  GstElement *sink,*src;
+  
+  /* init gstreamer */
+  gst_init (&argc, &argv);
+  bin = gst_pipeline_new ("song");
+  src = gst_element_factory_make ("audiotestsrc", NULL);
+  sink = gst_element_factory_make ("jackaudiosink", NULL);
+  gst_bin_add_many (GST_BIN (bin), src, sink, NULL);
+  gst_element_link (src, sink);
+  gst_element_set_state (bin, GST_STATE_PLAYING);
+  
+  event_loop (bin);
+  
+  gst_element_set_state (bin, GST_STATE_NULL);
+  g_object_unref (bin);
+  return 0;
+}
