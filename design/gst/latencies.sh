eeee82c79d6672f9b4090be05a0ac2c6d69fb48d
Stefan Sauer <ensonic@users.sf.net>
2012-02-07 16:25:31 +0100

    latencies: improve the test script
    Now the pasuspender invocations work.

--- design/gst/latencies.sh
+++ design/gst/latencies.sh
@@ -1,33 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 export GST_DEBUG="latency:4"
 export GST_DEBUG_NO_COLOR=1
 
+bpm=125
+tpb=8
+
 test="latency_alsasink_def"
-./latency 125 8 "alsasink" 2>${test}.dbg
+./latency $bpm $tpb "alsasink" 2>${test}.dbg
 ./latency.sh $test | gnuplot
 echo "[$test] done"
 
 test="latency_pulsesink_push"
-./latency 125 8 "pulsesink" 2>${test}.dbg
+./latency $bpm $tpb "pulsesink" 2>${test}.dbg
 ./latency.sh $test | gnuplot
 echo "[$test] done"
 
 test="latency_pulsesink_pull"
-./latency 125 8 "pulsesink can-activate-pull=true" 2>${test}.dbg
+./latency $bpm $tpb "pulsesink can-activate-pull=true" 2>${test}.dbg
 ./latency.sh $test | gnuplot
 echo "[$test] done"
 
 # needs pasuspend to work
 
-test="latency_alsasink_plughw"
-pasuspender -- ./latency 125 8 "alsasink device=plughw:0" 2>${test}.dbg
+test="latency_alsasink_hw"
+pasuspender -- ./latency $bpm $tpb "alsasink device=hw:0" 2>${test}.dbg
 ./latency.sh $test | gnuplot
 echo "[$test] done"
 
-#test="latency_jacksink"
-#pasuspender -- jackd --temporary -d alsa &
-#./latency 125 8 "jackaudiosink" 2>${test}.dbg
-#./latency.sh $test | gnuplot
-#echo "[$test] done"
+test="latency_alsasink_plughw"
+pasuspender -- ./latency $bpm $tpb "alsasink device=plughw:0" 2>${test}.dbg
+./latency.sh $test | gnuplot
+echo "[$test] done"
+
+test="latency_jacksink"
+pasuspender -- jackd >/dev/null --silent --temporary -d alsa &
+sleep 1s
+./latency $bpm $tpb "jackaudiosink" 2>${test}.dbg
+./latency.sh $test | gnuplot
+echo "[$test] done"
 
