fea6a50a4911b59dc9eeac15b9d626899af8557b
Stefan Sauer <ensonic@users.sf.net>
2012-02-09 21:45:08 +0100

    tools: use new gst api conditionally

--- src/lib/core/tools.c
+++ src/lib/core/tools.c
@@ -150,7 +150,9 @@ GList *bt_gst_check_elements(GList *list) {
   GstRegistry *registry;
   GstPluginFeature*feature;
   
+#if GST_CHECK_VERSION(0,10,31))
   g_return_val_if_fail(gst_is_initialized(),NULL);
+#endif
 
   if((registry=gst_registry_get_default())) {
 		for(node=list;node;node=g_list_next(node)) {
