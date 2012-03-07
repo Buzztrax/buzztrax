9531eed4bdf2f075bd9fbaad4fb0aa380099c5bf
Stefan Sauer <ensonic@users.sf.net>
2012-03-07 11:44:00 +0100

    playback-controller-settings: prepare the UI to support additional controllers

--- src/ui/edit/settings-page-playback-controller.c
+++ src/ui/edit/settings-page-playback-controller.c
@@ -40,8 +40,10 @@
  *   - then we can only start it for devices that have it, when they are plugged
  *     - list of devices + learn button on the settings page?
  *     - each one can be enabled or disabled (M/S flags from parent apply)
- * - need lots of new settings :/
- *   - GConf does not have enums or flags (save as int={0...3}) 
+ * - need more settings :/
+ *   - GConf does not have enums or flags
+ *     - save as int={0...3})
+ *     - save as two booleans (better, as thats waht we do already)
  */
 
 #define BT_EDIT
@@ -50,10 +52,18 @@
 #include "bt-edit.h"
 
 enum {
-  //DEVICE_MENU_ICON=0,
-  DEVICE_MENU_LABEL=0,
-  DEVICE_MENU_DEVICE
+  CONTROLLER_LIST_ID=0,
+  CONTROLLER_LIST_LABEL,
+  CONTROLLER_LIST_CAN_MASTER,
+  CONTROLLER_LIST_MASTER,
+  CONTROLLER_LIST_CAN_SLAVE,
+  CONTROLLER_LIST_SLAVE
 };
+
+typedef enum {
+  CONTROLLER_PAGE_UPNP=0,
+  CONTROLLER_PAGE_JACK
+} BtPlaybackControllerPage;
 
 struct _BtSettingsPagePlaybackControllerPrivate {
   /* used to validate if dispose has run */
@@ -64,6 +74,11 @@ struct _BtSettingsPagePlaybackControllerPrivate {
   
   BtSettings *settings;
 
+  BtPlaybackControllerPage page;
+  GtkTreeView *controller_list;
+  GtkNotebook *controller_pages;
+
+  /* coherence upnp */
   GtkWidget *port_entry;
 };
 
@@ -74,17 +89,74 @@ G_DEFINE_TYPE (BtSettingsPagePlaybackController, bt_settings_page_playback_contr
 
 //-- event handler
 
-static void on_activate_toggled(GtkToggleButton *togglebutton, gpointer user_data) {
+static void on_controller_list_cursor_changed(GtkTreeView *treeview,gpointer user_data) {
   BtSettingsPagePlaybackController *self=BT_SETTINGS_PAGE_PLAYBACK_CONTROLLER(user_data);
-  gboolean active=gtk_toggle_button_get_active(togglebutton);
-  gboolean active_setting;
+  GtkTreeSelection *selection;
+  GtkTreeModel     *model;
+  GtkTreeIter       iter;
 
-  g_object_get(self->priv->settings,"coherence-upnp-active",&active_setting,NULL);
-  GST_INFO("upnp active changes from %d -> %d",active_setting,active);
-  if(active!=active_setting) {
-    g_object_set(self->priv->settings,"coherence-upnp-active",active,NULL);
+  GST_INFO("settings list cursor changed");
+  selection=gtk_tree_view_get_selection(self->priv->controller_list);
+  if(gtk_tree_selection_get_selected(selection, &model, &iter)) {
+    guint id;
+
+    gtk_tree_model_get(model,&iter,CONTROLLER_LIST_ID,&id,-1);
+    GST_INFO("selected entry id %lu",id);
+    gtk_notebook_set_current_page(self->priv->controller_pages,id);
+    self->priv->page=id;
   }
-  gtk_widget_set_sensitive(self->priv->port_entry,active);
+}
+
+static void on_activate_master_toggled(GtkCellRendererToggle *cell_renderer,gchar *path, gpointer user_data) {
+  BtSettingsPagePlaybackController *self=BT_SETTINGS_PAGE_PLAYBACK_CONTROLLER(user_data);
+  GtkTreeIter iter;
+  GtkTreeModel *store;
+  gboolean active; // gtk_cell_renderer_toggle_get_active(cell_renderer); <- this does not seem to work
+  //gboolean active_setting;
+
+  store=gtk_tree_view_get_model(self->priv->controller_list);
+  gtk_tree_model_get_iter_from_string(store,&iter,path);
+  gtk_tree_model_get(store,&iter,CONTROLLER_LIST_MASTER,&active,-1);
+  active ^= 1;
+  gtk_list_store_set(GTK_LIST_STORE(store),&iter,CONTROLLER_LIST_MASTER,active,-1);
+
+  switch(self->priv->page) {
+    case CONTROLLER_PAGE_UPNP:
+      break;
+    case CONTROLLER_PAGE_JACK:
+      //g_object_get(self->priv->settings,"jack-transport-master",&active_setting,NULL);
+      //GST_INFO("jack transport master changes from %d -> %d",active_setting,active);
+      break;
+  }
+}
+
+static void on_activate_slave_toggled(GtkCellRendererToggle *cell_renderer,gchar *path, gpointer user_data) {
+  BtSettingsPagePlaybackController *self=BT_SETTINGS_PAGE_PLAYBACK_CONTROLLER(user_data);
+  GtkTreeIter iter;
+  GtkTreeModel *store;
+  gboolean active; // gtk_cell_renderer_toggle_get_active(cell_renderer); <- this does not seem to work
+  gboolean active_setting;
+  
+  store=gtk_tree_view_get_model(self->priv->controller_list);
+  gtk_tree_model_get_iter_from_string(store,&iter,path);
+  gtk_tree_model_get(store,&iter,CONTROLLER_LIST_SLAVE,&active,-1);
+  active ^= 1;
+  gtk_list_store_set(GTK_LIST_STORE(store),&iter,CONTROLLER_LIST_SLAVE,active,-1);
+
+  switch(self->priv->page) {
+    case CONTROLLER_PAGE_UPNP:
+      g_object_get(self->priv->settings,"coherence-upnp-active",&active_setting,NULL);
+      GST_INFO("upnp active changes from %d -> %d",active_setting,active);
+      if(active!=active_setting) {
+        g_object_set(self->priv->settings,"coherence-upnp-active",active,NULL);
+      }
+      gtk_widget_set_sensitive(self->priv->port_entry,active);
+      break;
+    case CONTROLLER_PAGE_JACK:
+      //g_object_get(self->priv->settings,"jack-transport-slave",&active_setting,NULL);
+      //GST_INFO("jack transport slave changes from %d -> %d",active_setting,active);
+      break;
+  }
 }
 
 static void on_port_changed(GtkSpinButton *spinbutton,gpointer user_data) {
@@ -96,11 +168,15 @@ static void on_port_changed(GtkSpinButton *spinbutton,gpointer user_data) {
 //-- helper methods
 
 static void bt_settings_page_playback_controller_init_ui(const BtSettingsPagePlaybackController *self) {
-  GtkWidget *label,*spacer,*widget;
+  GtkWidget *label,*spacer,*table,*scrolled_window;
+  GtkCellRenderer *renderer;
+  GtkListStore *store;
+  GtkTreeSelection *selection;
+  GtkTreeIter iter;
   GtkAdjustment *spin_adjustment;
   gboolean active;
   guint port;
-  gchar *str;
+  gchar *str,*plugin_name;
 
   gtk_widget_set_name(GTK_WIDGET(self),"playback controller settings");
 
@@ -117,33 +193,103 @@ static void bt_settings_page_playback_controller_init_ui(const BtSettingsPagePla
   gtk_table_attach(GTK_TABLE(self),label, 0, 3, 0, 1,  GTK_FILL|GTK_EXPAND, GTK_SHRINK, 2,1);
   gtk_table_attach(GTK_TABLE(self),spacer, 0, 1, 1, 4, GTK_SHRINK,GTK_SHRINK, 2,1);
 
-  // don't translate, this is a product
-  label=gtk_label_new("Coherence UPnP");
-  gtk_misc_set_alignment(GTK_MISC(label),1.0,0.5);
-  gtk_table_attach(GTK_TABLE(self),label, 1, 2, 1, 2, GTK_FILL,GTK_SHRINK, 2,1);
+  // add a list of playback controllers
+  scrolled_window=gtk_scrolled_window_new(NULL,NULL);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled_window),GTK_POLICY_NEVER,GTK_POLICY_AUTOMATIC);
+  gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolled_window),GTK_SHADOW_ETCHED_IN);
+  self->priv->controller_list=GTK_TREE_VIEW(gtk_tree_view_new());
+  g_signal_connect(self->priv->controller_list,"cursor-changed",G_CALLBACK(on_controller_list_cursor_changed),(gpointer)self);
 
-  widget=gtk_check_button_new();
-  gtk_table_attach(GTK_TABLE(self),widget, 2, 3, 1, 2, GTK_FILL|GTK_EXPAND,GTK_SHRINK, 2,1);
+  renderer=gtk_cell_renderer_text_new();
+  gtk_cell_renderer_text_set_fixed_height_from_font(GTK_CELL_RENDERER_TEXT(renderer), 1);
+  gtk_tree_view_insert_column_with_attributes(self->priv->controller_list,-1,_("Controller"),renderer,
+    "text",CONTROLLER_LIST_LABEL,
+    NULL);
+  renderer=gtk_cell_renderer_toggle_new();
+  gtk_tree_view_insert_column_with_attributes(self->priv->controller_list,-1,_("Master"),renderer,
+    "sensitive",CONTROLLER_LIST_CAN_MASTER,
+    "active",CONTROLLER_LIST_MASTER,
+    NULL);
+  g_signal_connect(renderer, "toggled", G_CALLBACK(on_activate_master_toggled), (gpointer)self);
+  renderer=gtk_cell_renderer_toggle_new();
+  gtk_tree_view_insert_column_with_attributes(self->priv->controller_list,-1,_("Slave"),renderer,
+    "sensitive",CONTROLLER_LIST_CAN_SLAVE,
+    "active",CONTROLLER_LIST_SLAVE,
+    NULL);
+  g_signal_connect(renderer, "toggled", G_CALLBACK(on_activate_slave_toggled), (gpointer)self);
+  
+  selection=gtk_tree_view_get_selection(self->priv->controller_list);
+  gtk_tree_selection_set_mode(selection,GTK_SELECTION_SINGLE);
+  gtk_container_add(GTK_CONTAINER(scrolled_window),GTK_WIDGET(self->priv->controller_list));
+  gtk_table_attach(GTK_TABLE(self),GTK_WIDGET(scrolled_window), 1, 3, 1, 2, GTK_FILL|GTK_EXPAND,GTK_FILL|GTK_EXPAND, 2,1);
+  
+  // add data model
+  store=gtk_list_store_new(6,G_TYPE_UINT,G_TYPE_STRING,G_TYPE_BOOLEAN,G_TYPE_BOOLEAN,G_TYPE_BOOLEAN,G_TYPE_BOOLEAN);
+  gtk_list_store_append(store, &iter);
+  gtk_list_store_set(store, &iter,
+                    CONTROLLER_LIST_ID,    CONTROLLER_PAGE_UPNP,
+                    // don't translate, this is a product
+                    CONTROLLER_LIST_LABEL, "Coherence UPnP",
+                    CONTROLLER_LIST_CAN_MASTER, FALSE,
+                    CONTROLLER_LIST_MASTER, FALSE,
+                    CONTROLLER_LIST_CAN_SLAVE, TRUE,
+                    CONTROLLER_LIST_SLAVE,  active,
+                    -1);
+  gtk_list_store_append(store, &iter);
+  gtk_list_store_set(store, &iter,
+                    CONTROLLER_LIST_ID,    CONTROLLER_PAGE_JACK,
+                    // don't translate, this is a product
+                    CONTROLLER_LIST_LABEL, "Jack Transport",
+                    CONTROLLER_LIST_CAN_MASTER, TRUE,
+                    CONTROLLER_LIST_MASTER, FALSE,
+                    CONTROLLER_LIST_CAN_SLAVE, TRUE,
+                    CONTROLLER_LIST_SLAVE,  FALSE,
+                    -1);
+
+  gtk_tree_view_set_model(self->priv->controller_list,GTK_TREE_MODEL(store));
+  gtk_tree_model_get_iter_first(GTK_TREE_MODEL(store),&iter);
+  gtk_tree_selection_select_iter(selection,&iter);
+  g_object_unref(store); // drop with treeview
+  
+  // add a notebook widget with hidden tabs
+  self->priv->controller_pages=GTK_NOTEBOOK(gtk_notebook_new());
+  gtk_notebook_set_show_tabs(self->priv->controller_pages,FALSE);
+  gtk_notebook_set_show_border(self->priv->controller_pages,FALSE);
+  gtk_table_attach(GTK_TABLE(self),GTK_WIDGET(self->priv->controller_pages), 1, 3, 2, 3, GTK_FILL,GTK_FILL, 2,1);
+
+
+  // coherence upnp tab
+  table=gtk_table_new(2,2,FALSE);
+  gtk_container_add(GTK_CONTAINER(self->priv->controller_pages),table);
 
   // local network port number for socket communication
   label=gtk_label_new(_("Port number"));
   gtk_misc_set_alignment(GTK_MISC(label),1.0,0.5);
-  gtk_table_attach(GTK_TABLE(self),label, 1, 2, 2, 3, GTK_FILL,GTK_SHRINK, 2,1);
+  gtk_table_attach(GTK_TABLE(table),label, 0, 1, 0, 1, GTK_FILL,GTK_SHRINK, 2,1);
 
   spin_adjustment=GTK_ADJUSTMENT(gtk_adjustment_new((gdouble)port, 1024.0, 65536.0, 1.0, 5.0, 0.0));
   self->priv->port_entry=gtk_spin_button_new(spin_adjustment,1.0,0);
-  gtk_table_attach(GTK_TABLE(self),self->priv->port_entry, 2, 3, 2, 3, GTK_FILL|GTK_EXPAND,GTK_SHRINK, 2,1);
+  g_signal_connect(self->priv->port_entry, "value-changed", G_CALLBACK(on_port_changed), (gpointer)self);
+  gtk_table_attach(GTK_TABLE(table),self->priv->port_entry, 1, 2, 0, 1, GTK_FILL|GTK_EXPAND,GTK_SHRINK, 2,1);
 
   // add coherence URL
   label=gtk_label_new("Requires Coherence UPnP framework which can be found at: https://coherence.beebits.net.");
   gtk_label_set_line_wrap(GTK_LABEL(label),TRUE);
   gtk_label_set_selectable(GTK_LABEL(label),TRUE);
-  gtk_table_attach(GTK_TABLE(self),label, 1, 3, 3, 4, GTK_FILL,GTK_SHRINK, 2,1);
+  gtk_table_attach(GTK_TABLE(table),label, 0, 2, 1, 2, GTK_FILL,GTK_SHRINK, 2,1);
 
-  // set current settings
-  g_signal_connect(widget, "toggled", G_CALLBACK(on_activate_toggled), (gpointer)self);
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(widget),active);
-  g_signal_connect(self->priv->port_entry, "value-changed", G_CALLBACK(on_port_changed), (gpointer)self);
+
+  // jack transport tab
+  plugin_name=bt_settings_determine_audiosink_name(self->priv->settings);
+  str=g_strdup_printf("Jack transport requires that the jackaudiosink is active."
+      "Your current audiosink is '%s'.", plugin_name);
+  label=gtk_label_new(str);
+  gtk_label_set_line_wrap(GTK_LABEL(label),TRUE);
+  gtk_label_set_selectable(GTK_LABEL(label),TRUE);
+  gtk_container_add(GTK_CONTAINER(self->priv->controller_pages),label);
+  g_free(plugin_name);
+  g_free(str);
+
 }
 
 //-- constructor methods
