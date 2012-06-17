06d79c3d3ee8a64b4c52059d75a9b1836a897c8f
Stefan Sauer <ensonic@users.sf.net>
2012-06-17 21:37:45 +0200

    tests: add 't' suite for processor machine

--- tests/lib/core/s-processor-machine.c
+++ tests/lib/core/s-processor-machine.c
@@ -19,13 +19,13 @@
 
 #include "m-bt-core.h"
 
-//extern TCase *bt_processor_machine_test_case(void);
+extern TCase *bt_processor_machine_test_case(void);
 extern TCase *bt_processor_machine_example_case(void);
 
 Suite *bt_processor_machine_suite(void) { 
   Suite *s=suite_create("BtProcessorMachine"); 
 
-  //suite_add_tcase(s,bt_processor_machine_test_case());
+  suite_add_tcase(s,bt_processor_machine_test_case());
   suite_add_tcase(s,bt_processor_machine_example_case());
   return(s);
 }
