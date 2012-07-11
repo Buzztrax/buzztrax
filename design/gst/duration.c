b1c749056dbe5ec6c26ee639b9d430de3c421f1d
Stefan Sauer <ensonic@users.sf.net>
2012-07-11 22:31:18 +0200

    design: another test app to try to configure a duration on a synthetic pipeline

---
+++ design/gst/duration.c
@@ -0,0 +1,94 @@
+/* Record n seconds of beep to an ogg file and checks duration queries
+ *
+ * gcc -Wall -g `pkg-config gstreamer-0.10 --cflags --libs` duration.c -o duration
+ *
+ * the duration query seems to only report a duration if it works for *all*
+ * sources, as soon as one source return -1, it returns -1
+ */
+
+#include <gst/gst.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void
+event_loop (GstElement * bin)
+{
+  GstBus *bus = gst_element_get_bus (GST_ELEMENT (bin));
+  GstMessage *message = NULL;
+
+  while (TRUE) {
+    message = gst_bus_poll (bus, GST_MESSAGE_ANY, -1);   
+    GST_INFO("message %s received",GST_MESSAGE_TYPE_NAME(message));
+
+    switch (message->type) {
+      case GST_MESSAGE_EOS:
+        gst_message_unref (message);
+        return;
+      case GST_MESSAGE_WARNING:
+      case GST_MESSAGE_ERROR: {
+        GError *gerror;
+        gchar *debug;
+
+        gst_message_parse_error (message, &gerror, &debug);
+        gst_object_default_error (GST_MESSAGE_SRC (message), gerror, debug);
+        gst_message_unref (message);
+        g_error_free (gerror);
+        g_free (debug);
+        return;
+      }
+      default:
+        gst_message_unref (message);
+        break;
+    }
+  }
+}
+
+gint
+main (gint argc, gchar ** argv)
+{
+  GstElement *bin, *src;
+  GstFormat query_fmt = GST_FORMAT_TIME;
+  gint64 duration;
+  gchar *name = "s0";
+
+  gst_init (&argc, &argv);
+    
+  bin = gst_parse_launch (
+    "audiotestsrc name=s0 ! adder name=mix ! vorbisenc ! oggmux ! filesink location=duration.ogg "
+    "audiotestsrc name=s1 ! mix. audiotestsrc name=s2 ! mix."
+    /*
+    "audiotestsrc name=s0 num-buffers=10000 ! adder name=mix ! vorbisenc ! oggmux ! filesink location=duration.ogg "
+    "audiotestsrc name=s1 num-buffers=10000  ! mix. audiotestsrc name=s2 num-buffers=10000 ! mix."
+    */
+    , NULL);
+  
+  if (argc>1) {
+    name = argv[1];
+  }
+  
+  src = gst_bin_get_by_name (GST_BIN (bin), name);
+  if (src) {
+    g_object_set (src, "num-buffers", 1000, NULL);
+    gst_object_unref (src);
+  } else {
+    puts("unknown src, not setting duration");
+  }
+  
+  gst_element_set_state (GST_ELEMENT (bin), GST_STATE_PAUSED);
+  if(!gst_element_query_duration (GST_ELEMENT (bin), &query_fmt, &duration)) {
+    puts("duration query failed");
+  } else {
+    if (duration==GST_CLOCK_TIME_NONE) {
+      puts("duration: unknown");
+    } else {
+      printf("duration: %s :%" GST_TIME_FORMAT "\n", 
+          gst_format_get_name (query_fmt), GST_TIME_ARGS (duration));
+    }
+  }
+  gst_element_set_state (GST_ELEMENT (bin), GST_STATE_PLAYING);
+  event_loop (bin);
+  gst_element_set_state (bin, GST_STATE_NULL);
+  gst_object_unref (GST_OBJECT (bin));
+  return 0;
+}
