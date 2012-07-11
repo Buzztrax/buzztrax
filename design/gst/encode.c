6baa534a38cf3f6b11d9738a093ff05d97d19923
Stefan Sauer <ensonic@users.sf.net>
2012-07-11 11:09:06 +0200

    design: check duration as well

--- design/gst/encode.c
+++ design/gst/encode.c
@@ -50,7 +50,9 @@ main (gint argc, gchar ** argv)
   GstElement *bin;
   gchar *pipeline = NULL;
   gint format = 0;
-  
+  GstFormat query_fmt = GST_FORMAT_TIME;
+  gint64 duration;
+
   gst_init (&argc, &argv);
   
   if(argc>1) {
@@ -59,21 +61,27 @@ main (gint argc, gchar ** argv)
   switch(format) {
     case 0:
       pipeline = "audiotestsrc ! vorbisenc ! oggmux ! filesink location=encode.ogg";
+      puts("encoding ogg vorbis");
       break;
     case 1:
       pipeline = "audiotestsrc ! lamemp3enc ! filesink location=encode.mp3";
+      puts("encoding mp3");
       break;
     case 2:
       pipeline = "audiotestsrc ! wavenc ! filesink location=encode.wav";
+      puts("encoding wav");
       break;
     case 3:
       pipeline = "audiotestsrc ! flacenc ! oggmux ! filesink location=encode.flac";
+      puts("encoding ogg flac");
       break;
     case 4:
       pipeline = "audiotestsrc ! faac ! mp4mux ! filesink location=encode.m4a";
+      puts("encoding m4a");
       break;
     case 5:
       pipeline = "audiotestsrc ! filesink location=encode.raw";
+      puts("encoding raw");
       break;
     default:
       puts("format must be 0-5");
@@ -81,13 +89,20 @@ main (gint argc, gchar ** argv)
   }
   bin = gst_parse_launch (pipeline, NULL);
   
-  gst_element_set_state (GST_ELEMENT (bin),GST_STATE_PAUSED);
+  gst_element_set_state (GST_ELEMENT (bin), GST_STATE_PAUSED);
   if(!gst_element_seek (bin, 1.0, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH,
       GST_SEEK_TYPE_SET, GST_SECOND*0,
       GST_SEEK_TYPE_SET, GST_SECOND*1))
     puts("seek failed");
   
-  gst_element_set_state (GST_ELEMENT (bin),GST_STATE_PLAYING);
+  gst_element_set_state (GST_ELEMENT (bin), GST_STATE_PLAYING);
+  
+  if(!gst_element_query_duration (GST_ELEMENT (bin), &query_fmt, &duration)) {
+    puts("duration query failed");
+  } else {
+    printf("duration: %s :%" GST_TIME_FORMAT "\n", gst_format_get_name (query_fmt),
+        GST_TIME_ARGS (duration));
+  }
   event_loop (bin);
   gst_element_set_state (bin, GST_STATE_NULL);
   gst_object_unref (GST_OBJECT (bin));
