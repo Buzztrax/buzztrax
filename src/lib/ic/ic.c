cf55c5060421bd5baa8baaba2cb3933fb0026891
Stefan Sauer <ensonic@users.sf.net>
2012-12-19 17:32:35 +0100

    copyright: replace FSF adress with a link to the webpage.
    This is also done by e.g. autotools and avoids to point to state information.

--- src/lib/ic/ic.c
+++ src/lib/ic/ic.c
@@ -12,9 +12,7 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 /**
  * SECTION:libbtic
@@ -35,28 +33,30 @@
  *
  * buzztard version stamp, major part; determined from #BTIC_MAJOR_VERSION
  */
-const guint btic_major_version=BTIC_MAJOR_VERSION;
+const guint btic_major_version = BTIC_MAJOR_VERSION;
 /**
  * btic_minor_version:
  *
  * buzztard version stamp, minor part; determined from #BTIC_MINOR_VERSION
  */
-const guint btic_minor_version=BTIC_MINOR_VERSION;
+const guint btic_minor_version = BTIC_MINOR_VERSION;
 /**
  * btic_micro_version:
  *
  * buzztard version stamp, micro part; determined from #BTIC_MICRO_VERSION
  */
-const guint btic_micro_version=BTIC_MICRO_VERSION;
+const guint btic_micro_version = BTIC_MICRO_VERSION;
 
-GST_DEBUG_CATEGORY(GST_CAT_DEFAULT);
+GST_DEBUG_CATEGORY (GST_CAT_DEFAULT);
 
 static gboolean arg_version = FALSE;
 
 //-- helper methods
 
 /* we have no fail cases yet, but maybe in the future */
-static gboolean btic_init_pre (void) {
+static gboolean
+btic_init_pre (void)
+{
 #ifdef ENABLE_NLS
   bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
@@ -69,14 +69,18 @@ static gboolean btic_init_pre (void) {
   return TRUE;
 }
 
-static gboolean btic_init_post (void) {
+static gboolean
+btic_init_post (void)
+{
   if (arg_version) {
-    g_printf("libbuzztard-ic-%d.%d.%d from "PACKAGE_STRING"\n",BTIC_MAJOR_VERSION,BTIC_MINOR_VERSION,BTIC_MICRO_VERSION);
+    g_printf ("libbuzztard-ic-%d.%d.%d from " PACKAGE_STRING "\n",
+        BTIC_MAJOR_VERSION, BTIC_MINOR_VERSION, BTIC_MICRO_VERSION);
   }
 
-  GST_DEBUG_CATEGORY_INIT(GST_CAT_DEFAULT, "bt-ic", 0, "music production environment / interaction controller library");
-  
-  return(TRUE);
+  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, "bt-ic", 0,
+      "music production environment / interaction controller library");
+
+  return (TRUE);
 }
 
 //-- ic initialisation
@@ -94,19 +98,25 @@ static gboolean btic_init_post (void) {
  *
  * Returns: a pointer to a GOption group. Should be dereferenced after use.
  */
-GOptionGroup *btic_init_get_option_group(void) {  
+GOptionGroup *
+btic_init_get_option_group (void)
+{
   GOptionGroup *group;
   static GOptionEntry options[] = {
-    {"btic-version", 0, 0, G_OPTION_ARG_NONE, NULL, N_("Print the buzztard interaction controller version"), NULL},
+    {"btic-version", 0, 0, G_OPTION_ARG_NONE, NULL,
+        N_("Print the buzztard interaction controller version"), NULL},
     {NULL}
   };
-  options[0].arg_data=&arg_version;
-  
-  group = g_option_group_new("bt-ic", _("Buzztard interaction controller options"),_("Show buzztard interaction controller options"), NULL, NULL);
-  g_option_group_set_parse_hooks(group, (GOptionParseFunc)btic_init_pre, (GOptionParseFunc)btic_init_post);
+  options[0].arg_data = &arg_version;
 
-  g_option_group_add_entries(group, options);
-  g_option_group_set_translation_domain(group, PACKAGE_NAME);
+  group =
+      g_option_group_new ("bt-ic", _("Buzztard interaction controller options"),
+      _("Show buzztard interaction controller options"), NULL, NULL);
+  g_option_group_set_parse_hooks (group, (GOptionParseFunc) btic_init_pre,
+      (GOptionParseFunc) btic_init_post);
+
+  g_option_group_add_entries (group, options);
+  g_option_group_set_translation_domain (group, PACKAGE_NAME);
 
   return group;
 }
@@ -125,16 +135,18 @@ GOptionGroup *btic_init_get_option_group(void) {
  *
  * Returns: %TRUE if Buzztard interaction controller could be initialized.
  */
-gboolean btic_init_check(gint *argc, gchar **argv[], GError **err) {
+gboolean
+btic_init_check (gint * argc, gchar ** argv[], GError ** err)
+{
   GOptionContext *ctx;
   gboolean res;
 
-  ctx = g_option_context_new(NULL);
-  g_option_context_add_group(ctx, btic_init_get_option_group());
-  res = g_option_context_parse(ctx, argc, argv, err);
-  g_option_context_free(ctx);
+  ctx = g_option_context_new (NULL);
+  g_option_context_add_group (ctx, btic_init_get_option_group ());
+  res = g_option_context_parse (ctx, argc, argv, err);
+  g_option_context_free (ctx);
 
-  return(res);
+  return (res);
 }
 
 
@@ -156,14 +168,17 @@ gboolean btic_init_check(gint *argc, gchar **argv[], GError **err) {
  * particular, unknown command line options cause this function to
  * abort program execution.
  */
-void btic_init(gint *argc, gchar **argv[]) {
+void
+btic_init (gint * argc, gchar ** argv[])
+{
   GError *err = NULL;
 
-  if(!btic_init_check(argc, argv, &err)) {
-    g_print("Could not initialized Buzztard interaction controller: %s\n", err ? err->message : "unknown error occurred");
-    if(err) {
-      g_error_free(err);
+  if (!btic_init_check (argc, argv, &err)) {
+    g_print ("Could not initialized Buzztard interaction controller: %s\n",
+        err ? err->message : "unknown error occurred");
+    if (err) {
+      g_error_free (err);
     }
-    exit(1);
+    exit (1);
   }
 }
