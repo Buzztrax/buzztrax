23c6293be55b2fa6fb302d89ceea3de0ce6f74b2
Stefan Sauer <ensonic@users.sf.net>
2014-02-22 21:51:33 +0100

    setup: move tests for edit-while-play from song to setup

--- tests/lib/core/e-setup.c
+++ tests/lib/core/e-setup.c
@@ -281,6 +281,224 @@ test_bt_setup_unique_machine_id1 (BT_TEST_ARGS)
   BT_TEST_END;
 }
 
+/*
+* check if we can connect a src machine to a sink while playing
+*/
+static void
+test_bt_setup_dynamic_add_src (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  /* arrange */
+  BtSong *song = bt_song_new (app);
+  BtSequence *sequence =
+      (BtSequence *) check_gobject_get_object_property (song, "sequence");
+  BtMachine *sink = BT_MACHINE (bt_sink_machine_new (song, "master", NULL));
+  BtMachine *gen1 =
+      BT_MACHINE (bt_source_machine_new (song, "gen1", "audiotestsrc", 0L,
+          NULL));
+  bt_wire_new (song, gen1, sink, NULL);
+  GstElement *element1 =
+      (GstElement *) check_gobject_get_object_property (gen1, "machine");
+
+  g_object_set (sequence, "length", 16L, NULL);
+  bt_sequence_add_track (sequence, gen1, -1);
+  g_object_set (element1, "wave", /* silence */ 4, NULL);
+
+  /* play the song */
+  if (bt_song_play (song)) {
+    mark_point ();
+    g_usleep (G_USEC_PER_SEC / 10);
+    GST_DEBUG ("song plays");
+
+    BtMachine *gen2 =
+        BT_MACHINE (bt_source_machine_new (song, "gen2", "audiotestsrc", 0L,
+            NULL));
+    GstElement *element2 =
+        (GstElement *) check_gobject_get_object_property (gen2, "machine");
+    g_object_set (element2, "wave", /* silence */ 4, NULL);
+    bt_wire_new (song, gen2, sink, NULL);
+    gst_object_unref (element2);
+
+    g_usleep (G_USEC_PER_SEC / 10);
+
+    /* stop the song */
+    bt_song_stop (song);
+  } else {
+    fail ("playing of song failed");
+  }
+
+  /* cleanup */
+  g_object_unref (sequence);
+  g_object_checked_unref (song);
+  BT_TEST_END;
+}
+
+/*
+* check if we can disconnect a src machine from a sink while playing.
+*/
+static void
+test_bt_setup_dynamic_rem_src (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  /* arrange */
+  BtSong *song = bt_song_new (app);
+  BtSetup *setup =
+      (BtSetup *) check_gobject_get_object_property (song, "setup");
+  BtSequence *sequence =
+      (BtSequence *) check_gobject_get_object_property (song, "sequence");
+  BtMachine *sink = BT_MACHINE (bt_sink_machine_new (song, "master", NULL));
+  BtMachine *gen1 =
+      BT_MACHINE (bt_source_machine_new (song, "gen1", "audiotestsrc", 0L,
+          NULL));
+  BtMachine *gen2 =
+      BT_MACHINE (bt_source_machine_new (song, "gen2", "audiotestsrc", 0L,
+          NULL));
+  bt_wire_new (song, gen1, sink, NULL);
+  BtWire *wire2 = bt_wire_new (song, gen2, sink, NULL);
+  GstElement *element1 =
+      (GstElement *) check_gobject_get_object_property (gen1, "machine");
+  GstElement *element2 =
+      (GstElement *) check_gobject_get_object_property (gen2, "machine");
+
+  g_object_set (sequence, "length", 16L, NULL);
+  bt_sequence_add_track (sequence, gen1, -1);
+  bt_sequence_add_track (sequence, gen2, -1);
+  g_object_set (element1, "wave", /* silence */ 4, NULL);
+  g_object_set (element2, "wave", /* silence */ 4, NULL);
+  mark_point ();
+
+  /* play the song */
+  if (bt_song_play (song)) {
+    mark_point ();
+    g_usleep (G_USEC_PER_SEC / 10);
+    GST_DEBUG ("song plays");
+
+    /* unlink machines */
+    bt_setup_remove_wire (setup, wire2);
+    GST_DEBUG ("wire removed");
+
+    g_usleep (G_USEC_PER_SEC / 10);
+
+    /* stop the song */
+    bt_song_stop (song);
+  } else {
+    fail ("playing of song failed");
+  }
+
+  /* cleanup */
+  g_object_unref (setup);
+  g_object_unref (sequence);
+  g_object_checked_unref (song);
+  BT_TEST_END;
+}
+
+/*
+ * check if we can connect a processor machine to a src and sink while playing
+ */
+static void
+test_bt_setup_dynamic_add_proc (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  /* arrange */
+  BtSong *song = bt_song_new (app);
+  BtSequence *sequence =
+      (BtSequence *) check_gobject_get_object_property (song, "sequence");
+  BtMachine *sink = BT_MACHINE (bt_sink_machine_new (song, "master", NULL));
+  BtMachine *gen1 =
+      BT_MACHINE (bt_source_machine_new (song, "gen1", "audiotestsrc", 0L,
+          NULL));
+  bt_wire_new (song, gen1, sink, NULL);
+  GstElement *element1 =
+      (GstElement *) check_gobject_get_object_property (gen1, "machine");
+
+  g_object_set (sequence, "length", 16L, NULL);
+  bt_sequence_add_track (sequence, gen1, -1);
+  g_object_set (element1, "wave", /* silence */ 4, NULL);
+  mark_point ();
+
+  /* play the song */
+  if (bt_song_play (song)) {
+    mark_point ();
+    g_usleep (G_USEC_PER_SEC / 10);
+    GST_DEBUG ("song plays");
+
+    BtMachine *proc =
+        BT_MACHINE (bt_processor_machine_new (song, "proc", "volume", 0, NULL));
+    bt_wire_new (song, gen1, proc, NULL);
+    bt_wire_new (song, proc, sink, NULL);
+
+    g_usleep (G_USEC_PER_SEC / 10);
+
+    /* stop the song */
+    bt_song_stop (song);
+  } else {
+    fail ("playing song failed");
+  }
+
+  /* cleanup */
+  g_object_unref (sequence);
+  g_object_checked_unref (song);
+  BT_TEST_END;
+}
+
+/*
+ * check if we can disconnect a processor machine to a src and sink while playing
+ */
+static void
+test_bt_setup_dynamic_rem_proc (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  /* arrange */
+  BtSong *song = bt_song_new (app);
+  BtSetup *setup =
+      (BtSetup *) check_gobject_get_object_property (song, "setup");
+  BtSequence *sequence =
+      (BtSequence *) check_gobject_get_object_property (song, "sequence");
+  BtMachine *sink = BT_MACHINE (bt_sink_machine_new (song, "master", NULL));
+  BtMachine *gen1 =
+      BT_MACHINE (bt_source_machine_new (song, "gen1", "audiotestsrc", 0L,
+          NULL));
+  BtMachine *proc =
+      BT_MACHINE (bt_processor_machine_new (song, "proc", "volume", 0, NULL));
+  bt_wire_new (song, gen1, proc, NULL);
+  BtWire *wire2 = bt_wire_new (song, gen1, proc, NULL);
+  BtWire *wire3 = bt_wire_new (song, proc, sink, NULL);
+  GstElement *element1 =
+      (GstElement *) check_gobject_get_object_property (gen1, "machine");
+
+  g_object_set (sequence, "length", 16L, NULL);
+  bt_sequence_add_track (sequence, gen1, -1);
+  g_object_set (element1, "wave", /* silence */ 4, NULL);
+  mark_point ();
+
+  /* play the song */
+  if (bt_song_play (song)) {
+    mark_point ();
+    g_usleep (G_USEC_PER_SEC / 10);
+    GST_DEBUG ("song plays");
+
+    /* unlink machines */
+    bt_setup_remove_wire (setup, wire2);
+    GST_DEBUG ("wire 2 removed");
+
+    bt_setup_remove_wire (setup, wire3);
+    GST_DEBUG ("wire 3 removed");
+
+    g_usleep (G_USEC_PER_SEC / 10);
+
+    /* stop the song */
+    bt_song_stop (song);
+  } else {
+    fail ("playing song failed");
+  }
+
+  /* cleanup */
+  g_object_unref (setup);
+  g_object_unref (sequence);
+  g_object_checked_unref (song);
+  BT_TEST_END;
+}
+
 TCase *
 bt_setup_example_case (void)
 {
@@ -296,6 +514,10 @@ bt_setup_example_case (void)
   tcase_add_test (tc, test_bt_setup_wire_add_dst_list);
   tcase_add_test (tc, test_bt_setup_machine_type);
   tcase_add_test (tc, test_bt_setup_unique_machine_id1);
+  tcase_add_test (tc, test_bt_setup_dynamic_add_src);
+  tcase_add_test (tc, test_bt_setup_dynamic_rem_src);
+  tcase_add_test (tc, test_bt_setup_dynamic_add_proc);
+  tcase_add_test (tc, test_bt_setup_dynamic_rem_proc);
   tcase_add_checked_fixture (tc, test_setup, test_teardown);
   tcase_add_unchecked_fixture (tc, case_setup, case_teardown);
   return (tc);
