d238a650de8d175ff5065b8c9e7f7e36964d2b96
Stefan Sauer <ensonic@users.sf.net>
2014-03-07 07:50:21 +0100

    dynlink: only delete out own graphs

--- design/gst/dynlink3.sh
+++ design/gst/dynlink3.sh
@@ -4,7 +4,7 @@ gcc -Wall -g dynlink3.c -o dynlink3 `pkg-config gstreamer-1.0 --cflags --libs`
 if test $? -ne 0; then
   exit 1
 fi
-rm *.{dot,png}
+rm dyn*.{dot,png}
 
 trap "echo ' aborted ...'" SIGINT
 GST_DEBUG="dynlink:2" GST_DEBUG_DUMP_DOT_DIR=$PWD ./dynlink3
--- design/gst/dynlink4.sh
+++ design/gst/dynlink4.sh
@@ -4,7 +4,7 @@ gcc -Wall -g dynlink4.c -o dynlink4 `pkg-config gstreamer-1.0 --cflags --libs`
 if test $? -ne 0; then
   exit 1
 fi
-rm *.{dot,png}
+rm dyn*.{dot,png}
 
 trap "echo ' aborted ...'" SIGINT
 GST_DEBUG="dynlink:2" GST_DEBUG_DUMP_DOT_DIR=$PWD ./dynlink4
