7a835876fb629d256fda7d0a9b4d580b2971c855
Stefan Sauer <ensonic@users.sf.net>
2014-03-25 20:18:42 +0100

    tests: export the DISPLAY var when we launched our own Xvfb only

--- tests/bt-edit-cli.sh
+++ tests/bt-edit-cli.sh
@@ -25,27 +25,25 @@ testOptions()
 # tests that launch the UI
 testStartup()
 {
-  if [ "$xvfb_pid" != "" ]; then
-    startSkipping
-  fi
-
-  DISPLAY=:9 $LIBTOOL $BUZZTRAX_EDIT &
+  $LIBTOOL $BUZZTRAX_EDIT &
   btedit_pid=$!
   sleep 1s && kill $btedit_pid
 }
 
 testStartupWithOptions()
 {
-  DISPLAY=:9 $LIBTOOL $BUZZTRAX_EDIT >/dev/null --command=test5 &
+  $LIBTOOL $BUZZTRAX_EDIT >/dev/null --command=test5 &
   btedit_pid=$!
   sleep 1s && kill $btedit_pid
 }
 
 oneTimeSetUp()
 {
+  # TODO(ensonic): use an existing Xvfb if possible, configure flag?
   if [ "$XVFB_PATH" != "" ]; then
     $XVFB_PATH :9 -ac -nolisten tcp -fp $XFONT_PATH -noreset -screen 0 1024x786x24 -render &
     xvfb_pid=$!
+    export DISPLAY=:9
   fi
 }
 
