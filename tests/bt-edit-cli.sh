584525f126c2dd1f697f74acc50ed7167140de22
Stefan Sauer <ensonic@users.sf.net>
2014-03-26 09:49:10 +0100

    tests: try -nolock for xvfb invocations to workaround drone.io issues
    Drone.io uses a version of overlayfs that has a bug wrt symlinks and that makes Xvfb fail to start.

--- tests/bt-edit-cli.sh
+++ tests/bt-edit-cli.sh
@@ -41,7 +41,7 @@ oneTimeSetUp()
 {
   # TODO(ensonic): use an existing Xvfb if possible, configure flag?
   if [ "$XVFB_PATH" != "" ]; then
-    $XVFB_PATH :9 -ac -nolisten tcp -fp $XFONT_PATH -noreset -screen 0 1024x786x24 -render &
+    $XVFB_PATH :9 -ac -nolisten tcp -fp $XFONT_PATH -noreset -screen 0 1024x786x24 -render -nolock &
     xvfb_pid=$!
     export DISPLAY=:9
   fi
