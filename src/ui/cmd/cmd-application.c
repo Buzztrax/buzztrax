1b59ff184ac9d5e81f0bb97296ece67fb3d5a9ff
Stefan Sauer <ensonic@users.sf.net>
2014-10-31 09:52:44 +0100

    buzztrax-cmd: add GError for song-io.
    Print the messages to stderr.
    Fixes #21 for buzztrax-cmd.

--- src/ui/cmd/cmd-application.c
+++ src/ui/cmd/cmd-application.c
@@ -359,6 +359,8 @@ bt_cmd_application_play (const BtCmdApplication * self,
   gboolean res = FALSE;
   BtSong *song = NULL;
   BtSongIO *loader = NULL;
+  GList *node, *missing_machines, *missing_waves;
+  GError *err = NULL;
 
   g_return_val_if_fail (BT_IS_CMD_APPLICATION (self), FALSE);
   g_return_val_if_fail (BT_IS_STRING (input_file_name), FALSE);
@@ -367,50 +369,49 @@ bt_cmd_application_play (const BtCmdApplication * self,
 
   // prepare song and song-io
   song = bt_cmd_application_song_init (self);
-  if (!(loader = bt_song_io_from_file (input_file_name, NULL))) {
+  if (!(loader = bt_song_io_from_file (input_file_name, &err))) {
+    g_fprintf (stderr, "could not create song-io for \"%s\": %s\n",
+        input_file_name, err->message);
+    g_error_free (err);
     goto Error;
   }
+  if (!bt_song_io_load (loader, song, &err)) {
+    g_fprintf (stderr, "could not load song \"%s\": %s\n", input_file_name,
+        err->message);
+    g_error_free (err);
+    goto Error;
+  }
+  // get missing element info
+  bt_child_proxy_get ((gpointer) song, "setup::missing-machines",
+      &missing_machines, "wavetable::missing-waves", &missing_waves, NULL);
 
-  GST_INFO ("objects initialized");
-
-  if (bt_song_io_load (loader, song, NULL)) {
-    GList *node, *missing_machines, *missing_waves;
-
-    // get missing element info
-    bt_child_proxy_get ((gpointer) song, "setup::missing-machines",
-        &missing_machines, "wavetable::missing-waves", &missing_waves, NULL);
-
-    if (missing_machines || missing_waves) {
-      printf ("could not load all of song\"%s\"\n", input_file_name);
+  if (missing_machines || missing_waves) {
+    g_fprintf (stderr, "could not load all of song\"%s\"\n", input_file_name);
+  }
+  if (missing_machines) {
+    g_fprintf (stderr, "missing machines\n");
+    for (node = missing_machines; node; node = g_list_next (node)) {
+      fprintf (stderr, "\t%s\n", (gchar *) (node->data));
     }
-    if (missing_machines) {
-      puts ("missing machines");
-      for (node = missing_machines; node; node = g_list_next (node)) {
-        printf ("  %s\n", (gchar *) (node->data));
-      }
+  }
+  if (missing_waves) {
+    g_fprintf (stderr, "missing waves\n");
+    for (node = missing_waves; node; node = g_list_next (node)) {
+      g_fprintf (stderr, "\t%s\n", (gchar *) (node->data));
     }
-    if (missing_waves) {
-      puts ("missing waves");
-      for (node = missing_waves; node; node = g_list_next (node)) {
-        printf ("  %s\n", (gchar *) (node->data));
-      }
-    }
+  }
 
-    GST_INFO ("start playback");
-    if (bt_cmd_application_play_song (self, song)) {
-      res = TRUE;
-    } else {
-      GST_ERROR ("could not play song \"%s\"", input_file_name);
-      goto Error;
-    }
+  GST_INFO ("start playback");
+  if (bt_cmd_application_play_song (self, song)) {
+    res = TRUE;
   } else {
-    GST_ERROR ("could not load song \"%s\"", input_file_name);
+    GST_ERROR ("could not play song \"%s\"", input_file_name);
     goto Error;
   }
 Error:
   g_object_try_unref (song);
   g_object_try_unref (loader);
-  return (res);
+  return res;
 }
 
 /**
@@ -432,6 +433,7 @@ bt_cmd_application_info (const BtCmdApplication * self,
   BtSong *song = NULL;
   BtSongIO *loader = NULL;
   FILE *output_file = NULL;
+  GError *err = NULL;
 
   g_return_val_if_fail (BT_IS_CMD_APPLICATION (self), FALSE);
   g_return_val_if_fail (BT_IS_STRING (input_file_name), FALSE);
@@ -443,138 +445,141 @@ bt_cmd_application_info (const BtCmdApplication * self,
     output_file = stdout;
   } else {
     if (!(output_file = fopen (output_file_name, "wb"))) {
-      GST_ERROR ("cannot open output file \"%s\"", output_file_name);
+      fprintf (stderr, "cannot open output file \"%s\": %s", output_file_name,
+          g_strerror (errno));
       goto Error;
     }
   }
   // prepare song and song-io
   song = bt_cmd_application_song_init (self);
-  if (!(loader = bt_song_io_from_file (input_file_name, NULL))) {
+  if (!(loader = bt_song_io_from_file (input_file_name, &err))) {
+    g_fprintf (stderr, "could not create song-io for \"%s\": %s\n",
+        input_file_name, err->message);
     goto Error;
   }
 
-  GST_INFO ("objects initialized");
+  if (!bt_song_io_load (loader, song, &err)) {
+    g_fprintf (stderr, "could not load song \"%s\": %s\n", input_file_name,
+        err->message);
+    g_error_free (err);
+    goto Error;
+  }
 
-  if (bt_song_io_load (loader, song, NULL)) {
-    BtSongInfo *song_info;
-    BtSequence *sequence;
-    BtSetup *setup;
-    BtWavetable *wavetable;
-    BtMachine *machine;
-    gchar *name, *info, *author, *genre, *id, *create_dts, *change_dts;
-    gulong beats_per_minute, ticks_per_beat;
-    gulong length, tracks, n_patterns = 0;
-    gboolean loop;
-    glong loop_start, loop_end;
-    GList *machines, *wires, *patterns, *waves, *node;
-    GList *missing_machines, *missing_waves;
-    GstBin *bin;
-    gulong msec, sec, min;
+  BtSongInfo *song_info;
+  BtSequence *sequence;
+  BtSetup *setup;
+  BtWavetable *wavetable;
+  BtMachine *machine;
+  gchar *name, *info, *author, *genre, *id, *create_dts, *change_dts;
+  gulong beats_per_minute, ticks_per_beat;
+  gulong length, tracks, n_patterns = 0;
+  gboolean loop;
+  glong loop_start, loop_end;
+  GList *machines, *wires, *patterns, *waves, *node;
+  GList *missing_machines, *missing_waves;
+  GstBin *bin;
+  gulong msec, sec, min;
 
-    //DEBUG
-    //bt_song_write_to_highlevel_dot_file(song);
-    //DEBUG
+  //DEBUG
+  //bt_song_write_to_highlevel_dot_file(song);
+  //DEBUG
 
-    g_object_get ((gpointer) song, "song-info", &song_info, "sequence",
-        &sequence, "setup", &setup, "wavetable", &wavetable, NULL);
-    // get missing element info
-    g_object_get (setup, "missing-machines", &missing_machines, NULL);
-    g_object_get (wavetable, "missing-waves", &missing_waves, NULL);
+  g_object_get ((gpointer) song, "song-info", &song_info, "sequence",
+      &sequence, "setup", &setup, "wavetable", &wavetable, NULL);
+  // get missing element info
+  g_object_get (setup, "missing-machines", &missing_machines, NULL);
+  g_object_get (wavetable, "missing-waves", &missing_waves, NULL);
 
-    // print some info about the song
-    g_object_get (song_info,
-        "name", &name, "author", &author, "genre", &genre, "info", &info,
-        "bpm", &beats_per_minute, "tpb", &ticks_per_beat,
-        "create-dts", &create_dts, "change-dts", &change_dts, NULL);
-    g_fprintf (output_file, "song.song_info.name: \"%s\"\n", name);
+  // print some info about the song
+  g_object_get (song_info,
+      "name", &name, "author", &author, "genre", &genre, "info", &info,
+      "bpm", &beats_per_minute, "tpb", &ticks_per_beat,
+      "create-dts", &create_dts, "change-dts", &change_dts, NULL);
+  g_fprintf (output_file, "song.song_info.name: \"%s\"\n", name);
+  g_free (name);
+  g_fprintf (output_file, "song.song_info.author: \"%s\"\n", author);
+  g_free (author);
+  g_fprintf (output_file, "song.song_info.genre: \"%s\"\n", genre);
+  g_free (genre);
+  g_fprintf (output_file, "song.song_info.info: \"%s\"\n", info);
+  g_free (info);
+  g_fprintf (output_file, "song.song_info.bpm: %lu\n", beats_per_minute);
+  g_fprintf (output_file, "song.song_info.tpb: %lu\n", ticks_per_beat);
+  g_fprintf (output_file, "song.song_info.created: \"%s\"\n", create_dts);
+  g_free (create_dts);
+  g_fprintf (output_file, "song.song_info.changed: \"%s\"\n", change_dts);
+  g_free (change_dts);
+  // print some info about the sequence
+  g_object_get (sequence, "length", &length, "tracks", &tracks, "loop", &loop,
+      "loop-start", &loop_start, "loop-end", &loop_end, NULL);
+  g_fprintf (output_file, "song.sequence.length: %lu\n", length);
+  g_fprintf (output_file, "song.sequence.tracks: %lu\n", tracks);
+  g_fprintf (output_file, "song.sequence.loop: %s\n", (loop ? "yes" : "no"));
+  g_fprintf (output_file, "song.sequence.loop-start: %ld\n", loop_start);
+  g_fprintf (output_file, "song.sequence.loop-end: %ld\n", loop_end);
+  // print playing-time
+  bt_song_info_tick_to_m_s_ms (song_info, length, &min, &sec, &msec);
+  g_fprintf (output_file, "song.sequence.playing_time: %02lu:%02lu.%03lu\n",
+      min, sec, msec);
+
+  // print some statistics about the song (number of machines, wires, patterns)
+  g_object_get (setup, "machines", &machines, "wires", &wires, NULL);
+  g_fprintf (output_file, "song.setup.number_of_machines: %u\n",
+      g_list_length (machines));
+  g_fprintf (output_file, "song.setup.number_of_wires: %u\n",
+      g_list_length (wires));
+  for (node = machines; node; node = g_list_next (node)) {
+    g_object_get (node->data, "patterns", &patterns, NULL);
+    // TODO(ensonic): this include internal ones
+    n_patterns += g_list_length (patterns);
+    g_list_foreach (patterns, (GFunc) g_object_unref, NULL);
+    g_list_free (patterns);
+  }
+  g_fprintf (output_file, "song.setup.number_of_patterns: %lu\n", n_patterns);
+  g_fprintf (output_file, "song.setup.number_of_missing_machines: %u\n",
+      g_list_length (missing_machines));
+  for (node = missing_machines; node; node = g_list_next (node)) {
+    g_fprintf (output_file, "  %s\n", (gchar *) (node->data));
+  }
+  g_list_free (machines);
+  g_list_free (wires);
+  g_object_get (wavetable, "waves", &waves, NULL);
+  g_fprintf (output_file, "song.wavetable.number_of_waves: %u\n",
+      g_list_length (waves));
+  g_fprintf (output_file, "song.wavetable.number_of_missing_waves: %u\n",
+      g_list_length (missing_waves));
+  for (node = missing_waves; node; node = g_list_next (node)) {
+    g_fprintf (output_file, "  %s\n", (gchar *) (node->data));
+  }
+  g_list_free (waves);
+  g_object_get ((gpointer) self, "bin", &bin, NULL);
+  g_fprintf (output_file, "app.bin.number_of_elements: %u\n",
+      GST_BIN_NUMCHILDREN (bin));
+  gst_object_unref (bin);
+
+  // lookup the audio-sink machine and print some info about it
+  if ((machine = bt_setup_get_machine_by_type (setup, BT_TYPE_SINK_MACHINE))) {
+    g_object_get (machine, "id", &id, "plugin_name", &name, NULL);
+    g_fprintf (output_file, "machine.id: \"%s\"\n", id);
+    g_free (id);
+    g_fprintf (output_file, "machine.plugin_name: \"%s\"\n", name);
     g_free (name);
-    g_fprintf (output_file, "song.song_info.author: \"%s\"\n", author);
-    g_free (author);
-    g_fprintf (output_file, "song.song_info.genre: \"%s\"\n", genre);
-    g_free (genre);
-    g_fprintf (output_file, "song.song_info.info: \"%s\"\n", info);
-    g_free (info);
-    g_fprintf (output_file, "song.song_info.bpm: %lu\n", beats_per_minute);
-    g_fprintf (output_file, "song.song_info.tpb: %lu\n", ticks_per_beat);
-    g_fprintf (output_file, "song.song_info.created: \"%s\"\n", create_dts);
-    g_free (create_dts);
-    g_fprintf (output_file, "song.song_info.changed: \"%s\"\n", change_dts);
-    g_free (change_dts);
-    // print some info about the sequence
-    g_object_get (sequence, "length", &length, "tracks", &tracks, "loop", &loop,
-        "loop-start", &loop_start, "loop-end", &loop_end, NULL);
-    g_fprintf (output_file, "song.sequence.length: %lu\n", length);
-    g_fprintf (output_file, "song.sequence.tracks: %lu\n", tracks);
-    g_fprintf (output_file, "song.sequence.loop: %s\n", (loop ? "yes" : "no"));
-    g_fprintf (output_file, "song.sequence.loop-start: %ld\n", loop_start);
-    g_fprintf (output_file, "song.sequence.loop-end: %ld\n", loop_end);
-    // print playing-time
-    bt_song_info_tick_to_m_s_ms (song_info, length, &min, &sec, &msec);
-    g_fprintf (output_file, "song.sequence.playing_time: %02lu:%02lu.%03lu\n",
-        min, sec, msec);
-
-    // print some statistics about the song (number of machines, wires, patterns)
-    g_object_get (setup, "machines", &machines, "wires", &wires, NULL);
-    g_fprintf (output_file, "song.setup.number_of_machines: %u\n",
-        g_list_length (machines));
-    g_fprintf (output_file, "song.setup.number_of_wires: %u\n",
-        g_list_length (wires));
-    for (node = machines; node; node = g_list_next (node)) {
-      g_object_get (node->data, "patterns", &patterns, NULL);
-      // TODO(ensonic): this include internal ones
-      n_patterns += g_list_length (patterns);
-      g_list_foreach (patterns, (GFunc) g_object_unref, NULL);
-      g_list_free (patterns);
-    }
-    g_fprintf (output_file, "song.setup.number_of_patterns: %lu\n", n_patterns);
-    g_fprintf (output_file, "song.setup.number_of_missing_machines: %u\n",
-        g_list_length (missing_machines));
-    for (node = missing_machines; node; node = g_list_next (node)) {
-      g_fprintf (output_file, "  %s\n", (gchar *) (node->data));
-    }
-    g_list_free (machines);
-    g_list_free (wires);
-    g_object_get (wavetable, "waves", &waves, NULL);
-    g_fprintf (output_file, "song.wavetable.number_of_waves: %u\n",
-        g_list_length (waves));
-    g_fprintf (output_file, "song.wavetable.number_of_missing_waves: %u\n",
-        g_list_length (missing_waves));
-    for (node = missing_waves; node; node = g_list_next (node)) {
-      g_fprintf (output_file, "  %s\n", (gchar *) (node->data));
-    }
-    g_list_free (waves);
-    g_object_get ((gpointer) self, "bin", &bin, NULL);
-    g_fprintf (output_file, "app.bin.number_of_elements: %u\n",
-        GST_BIN_NUMCHILDREN (bin));
-    gst_object_unref (bin);
-
-    // lookup the audio-sink machine and print some info about it
-    if ((machine = bt_setup_get_machine_by_type (setup, BT_TYPE_SINK_MACHINE))) {
-      g_object_get (machine, "id", &id, "plugin_name", &name, NULL);
-      g_fprintf (output_file, "machine.id: \"%s\"\n", id);
-      g_free (id);
-      g_fprintf (output_file, "machine.plugin_name: \"%s\"\n", name);
-      g_free (name);
-      g_object_unref (machine);
-    }
-    // release the references
-    g_object_unref (song_info);
-    g_object_unref (sequence);
-    g_object_unref (setup);
-    g_object_unref (wavetable);
-    res = TRUE;
-    GST_INFO ("finished successfully");
-  } else {
-    GST_ERROR ("could not load song \"%s\"", input_file_name);
-    goto Error;
+    g_object_unref (machine);
   }
+  // release the references
+  g_object_unref (song_info);
+  g_object_unref (sequence);
+  g_object_unref (setup);
+  g_object_unref (wavetable);
+  res = TRUE;
+  GST_INFO ("finished successfully");
 Error:
   g_object_try_unref (song);
   g_object_try_unref (loader);
   if (output_file) {
     fclose (output_file);
   }
-  return (res);
+  return res;
 }
 
 /**
