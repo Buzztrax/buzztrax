050041e7b5291282ca952bea6727abb74512ab56
Stefan Sauer <ensonic@users.sf.net>
2014-12-05 08:06:35 +0100

    tests: remove leftover commented code

--- tests/lib/ic/e-device.c
+++ tests/lib/ic/e-device.c
@@ -56,9 +56,6 @@ test_btic_device_lookup (BT_TEST_ARGS)
   GST_INFO ("-- arrange --");
 
   GST_INFO ("-- act --");
-  //GList *devices =
-  //    (GList *) check_gobject_get_ptr_property (registry, "devices");
-  //BtIcDevice *device = (BtIcDevice *) devices->data;
   BtIcDevice *device = btic_registry_find_device_by_name ("test");
 
   GST_INFO ("-- assert --");
