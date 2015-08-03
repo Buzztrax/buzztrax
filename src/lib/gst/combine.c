48228d285042af761eb65e356bde8d4c2759c44c
Stefan Sauer <ensonic@users.sf.net>
2015-08-03 18:22:38 +0200

    combine: add a mixing module
    The module provides several ways for combining two signals into on, among mixing
    and amplitude modulation.

---
+++ src/lib/gst/combine.c
@@ -0,0 +1,267 @@
+/* GStreamer
+ * Copyright (C) 2015 Stefan Sauer <ensonic@users.sf.net>
+ *
+ * combine.c: combine/mixing module
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTcombine.hICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * SECTION:combine
+ * @title: GstBtCombine
+ * @include: libgstbuzztrax/combine.h
+ * @short_description: combine/mixing module
+ *
+ * A module that combines 2 buffers using different (#GstBtCombine:type).
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "combine.h"
+
+#define GST_CAT_DEFAULT combine_debug
+GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
+
+enum
+{
+  // dynamic class properties
+  PROP_COMBINE = 1
+};
+
+//-- the class
+
+G_DEFINE_TYPE (GstBtCombine, gstbt_combine, GST_TYPE_OBJECT);
+
+//-- enumscombine.h
+
+GType
+gstbt_combine_type_get_type (void)
+{
+  static GType type = 0;
+  static const GEnumValue enums[] = {
+    {GSTBT_COMBINE_MIX, "Mix: A+B", "add both signals (A+B)"},
+    {GSTBT_COMBINE_MUL, "Mul: A*B",
+        "multiply signals (A*B) (amplitude modulation)"},
+    {GSTBT_COMBINE_SUB, "Sub: A-B", "subtract signals (A-B)"},
+    {GSTBT_COMBINE_MAX, "Max: Max(A,B)", "max of both  signals (max(A,B))"},
+    {GSTBT_COMBINE_MIN, "Min: Min(A,B)", "min of both  signals (min(A,B))"},
+    {0, NULL, NULL},
+  };
+
+  if (G_UNLIKELY (!type)) {
+    type = g_enum_register_static ("GstBtCombineType", enums);
+  }
+  return type;
+}
+
+//-- constructor methods
+
+/**
+ * gstbt_combine_new:
+ *
+ * Create a new instance
+ *
+ * Returns: the new instance or %NULL in case of an error
+ */
+GstBtCombine *
+gstbt_combine_new (void)
+{
+  return GSTBT_COMBINE (g_object_new (GSTBT_TYPE_COMBINE, NULL));
+}
+
+//-- private methods
+
+static void
+gstbt_combine_mix (GstBtCombine * self, guint ct, gint16 * d1, gint16 * d2)
+{
+  guint i;
+  glong mix;
+
+  for (i = 0; i < ct; i++) {
+    mix = (glong) d1[i] + (glong) d2[i];
+    d1[i] = (gint16) CLAMP (mix, G_MININT16, G_MAXINT16);
+  }
+}
+
+static void
+gstbt_combine_mul (GstBtCombine * self, guint ct, gint16 * d1, gint16 * d2)
+{
+  guint i;
+  glong mix;
+
+  for (i = 0; i < ct; i++) {
+    mix = ((glong) d1[i] * (glong) d2[i]) >> 15;
+    d1[i] = (gint16) CLAMP (mix, G_MININT16, G_MAXINT16);
+  }
+}
+
+static void
+gstbt_combine_sub (GstBtCombine * self, guint ct, gint16 * d1, gint16 * d2)
+{
+  guint i;
+  glong mix;
+
+  for (i = 0; i < ct; i++) {
+    mix = (glong) d1[i] - (glong) d2[i];
+    d1[i] = (gint16) CLAMP (mix, G_MININT16, G_MAXINT16);
+  }
+}
+
+static void
+gstbt_combine_max (GstBtCombine * self, guint ct, gint16 * d1, gint16 * d2)
+{
+  guint i;
+
+  for (i = 0; i < ct; i++) {
+    d1[i] = MAX (d1[i], d2[i]);
+  }
+}
+
+static void
+gstbt_combine_min (GstBtCombine * self, guint ct, gint16 * d1, gint16 * d2)
+{
+  guint i;
+
+  for (i = 0; i < ct; i++) {
+    d1[i] = MIN (d1[i], d2[i]);
+  }
+}
+
+/*
+ * gstbt_combine_change_type:
+ * Assign combine function
+ */
+static void
+gstbt_combine_change_type (GstBtCombine * self)
+{
+  switch (self->type) {
+    case GSTBT_COMBINE_MIX:
+      self->process = gstbt_combine_mix;
+      break;
+    case GSTBT_COMBINE_MUL:
+      self->process = gstbt_combine_mul;
+      break;
+    case GSTBT_COMBINE_SUB:
+      self->process = gstbt_combine_sub;
+      break;
+    case GSTBT_COMBINE_MAX:
+      self->process = gstbt_combine_max;
+      break;
+    case GSTBT_COMBINE_MIN:
+      self->process = gstbt_combine_min;
+      break;
+    default:
+      GST_ERROR ("invalid combine-type: %d", self->type);
+      break;
+  }
+}
+
+//-- public methods
+
+/**
+ * gstbt_combine_trigger:
+ * @self: the combine module
+ *
+ * Reset state. Typically called for new notes.
+ */
+void
+gstbt_combine_trigger (GstBtCombine * self)
+{
+  self->offset = 0;
+}
+
+/**
+ * gstbt_combine_process:
+ * @self: the oscillator
+ * @size: number of sample to filter
+ * @d1: buffer with the audio
+ * @d2: buffer with the audio
+ *
+ * Process @size samples of audio from @d1 and @d2. Stores the result into @d1.
+ */
+void
+gstbt_combine_process (GstBtCombine * self, guint size, gint16 * d1,
+    gint16 * d2)
+{
+  gst_object_sync_values ((GstObject *) self, self->offset);
+  self->process (self, size, d1, d2);
+  self->offset += size;
+}
+
+//-- virtual methods
+
+static void
+gstbt_combine_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstBtCombine *self = GSTBT_COMBINE (object);
+
+  switch (prop_id) {
+    case PROP_COMBINE:
+      //GST_INFO("change type %d -> %d",g_value_get_enum (value),self->filter);
+      self->type = g_value_get_enum (value);
+      gstbt_combine_change_type (self);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gstbt_combine_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstBtCombine *self = GSTBT_COMBINE (object);
+
+  switch (prop_id) {
+    case PROP_COMBINE:
+      g_value_set_enum (value, self->type);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gstbt_combine_init (GstBtCombine * self)
+{
+  self->type = GSTBT_COMBINE_MIX;
+  gstbt_combine_change_type (self);
+}
+
+static void
+gstbt_combine_class_init (GstBtCombineClass * klass)
+{
+  GObjectClass *gobject_class = (GObjectClass *) klass;
+
+  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, "combine",
+      GST_DEBUG_FG_WHITE | GST_DEBUG_BG_BLACK, "combine/mixing module");
+
+  gobject_class->set_property = gstbt_combine_set_property;
+  gobject_class->get_property = gstbt_combine_get_property;
+
+  // register own properties
+
+  g_object_class_install_property (gobject_class, PROP_COMBINE,
+      g_param_spec_enum ("combine", "Combinetype", "Type of combine operation",
+          GSTBT_TYPE_COMBINE_TYPE, GSTBT_COMBINE_MIX,
+          G_PARAM_READWRITE | GST_PARAM_CONTROLLABLE | G_PARAM_STATIC_STRINGS));
+}
---
+++ src/lib/gst/combine.h
@@ -0,0 +1,95 @@
+/* GStreamer
+ * Copyright (C) 2015 Stefan Sauer <ensonic@users.sf.net>
+ *
+ * combine.h: combine/mixing module
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __GSTBT_COMBINE_H__
+#define __GSTBT_COMBINE_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GSTBT_TYPE_COMBINE_TYPE (gstbt_combine_type_get_type())
+
+/**
+ * GstBtCombineType:
+ * @GSTBT_COMBINE_MIX: add both signals (A+B)
+ * @GSTBT_COMBINE_MUL: multiply signals (A*B) (amplitude modulation)
+ * @GSTBT_COMBINE_SUB: subtract signals (A-B)
+ * @GSTBT_COMBINE_MAX: max of both  signals (max(A,B))
+ * @GSTBT_COMBINE_MIN: min of both  signals (min(A,B))
+ *
+ * Combine types.
+ */
+typedef enum
+{
+  GSTBT_COMBINE_MIX,
+  GSTBT_COMBINE_MUL,
+  GSTBT_COMBINE_SUB,
+  GSTBT_COMBINE_MAX,
+  GSTBT_COMBINE_MIN
+} GstBtCombineType;
+
+GType gstbt_combine_type_get_type(void);
+
+
+#define GSTBT_TYPE_COMBINE            (gstbt_combine_get_type())
+#define GSTBT_COMBINE(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GSTBT_TYPE_COMBINE,GstBtCombine))
+#define GSTBT_IS_COMBINE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GSTBT_TYPE_COMBINE))
+#define GSTBT_COMBINE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass) ,GSTBT_TYPE_COMBINE,GstBtCombineClass))
+#define GSTBT_IS_COMBINE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass) ,GSTBT_TYPE_COMBINE))
+#define GSTBT_COMBINE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj) ,GSTBT_TYPE_COMBINE,GstBtCombineClass))
+
+typedef struct _GstBtCombine GstBtCombine;
+typedef struct _GstBtCombineClass GstBtCombineClass;
+
+/**
+ * GstBtCombine:
+ * @type: combine type
+ *
+ * Class instance data.
+ */
+struct _GstBtCombine {
+  GstObject parent;
+  /* < private > */
+  gboolean dispose_has_run;		/* validate if dispose has run */
+
+  /* < public > */
+  /* parameters */
+  GstBtCombineType type;
+
+  /* < private > */
+  guint64 offset;
+
+  /* < private > */
+  void (*process) (GstBtCombine *, guint, gint16 *, gint16 *);
+};
+
+struct _GstBtCombineClass {
+  GstObjectClass parent_class;
+};
+
+GType gstbt_combine_get_type(void);
+
+GstBtCombine *gstbt_combine_new(void);
+
+void gstbt_combine_trigger(GstBtCombine *self);
+void gstbt_combine_process(GstBtCombine *self, guint size, gint16 *d1, gint16 *d2);
+
+G_END_DECLS
+#endif /* __GSTBT_COMBINE_H__ */
