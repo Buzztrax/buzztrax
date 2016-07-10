2780c78cb329fefb55d2262bb27f07ff3a32b9c0
Stefan Sauer <ensonic@users.sf.net>
2016-07-10 21:53:17 +0200

    travis: install gdb

--- scripts/gdbtests.sh
+++ scripts/gdbtests.sh
@@ -5,7 +5,8 @@ log=$1
 IFS="
 "
 for l in `grep "Received signal" "$log"`; do
-  t=`echo $l | cut -d':' -f5`;
-  s=`echo $l | cut -d'/' -f3`;
-  BT_CHECKS="$t" make bt_$s.gdbrun;
+  t=`echo $l | cut -d':' -f5`
+  s=`echo $l | cut -d'/' -f3`
+  echo BT_CHECKS="$t" make bt_$s.gdbrun
+  BT_CHECKS="$t" make bt_$s.gdbrun
 done
