235c581a3438ea1ce32999fff9293543d60d8e41
Stefan Sauer <ensonic@users.sf.net>
2016-09-05 21:14:23 +0200

    tests: refactor tests as a loop test
    This way the assertions are in the test and not in the helper.

--- tests/lib/gst/e-elements.c
+++ tests/lib/gst/e-elements.c
@@ -95,70 +95,62 @@ done:
 
 //-- tests
 
+gchar *pipeline_desc[] = {
+  "filesrc location=%s ! buzztrax-dec ! fakesink sync=false",
+  "filesrc location=%s ! queue ! buzztrax-dec ! fakesink sync=false",
+  "playbin uri=file://%s audio-sink=\"fakesink sync=false\""
+};
+
 static void
-test_launch_bt_dec_pull (BT_TEST_ARGS)
+test_launch_bt_dec (BT_TEST_ARGS)
 {
   BT_TEST_START;
   GST_INFO ("-- arrange --");
-  gchar *str =
-      g_strdup_printf
-      ("filesrc location=%s ! buzztrax-dec ! fakesink sync=false",
+  gchar *str = g_strdup_printf (pipeline_desc[_i],
       check_get_test_song_path ("simple2.xml"));
   GstElement *pipeline = gst_parse_launch (str, NULL);
+  GstMessageType message_types =
+      GST_MESSAGE_NEW_CLOCK | GST_MESSAGE_STATE_CHANGED |
+      GST_MESSAGE_STREAM_STATUS | GST_MESSAGE_ASYNC_DONE |
+      GST_MESSAGE_STREAM_START | GST_MESSAGE_TAG;
+  GstMessageType tmessage = GST_MESSAGE_EOS;
 
   GST_INFO ("-- act && assert --");
-  fail_unless (run_pipeline (pipeline,
-          GST_MESSAGE_NEW_CLOCK | GST_MESSAGE_STATE_CHANGED |
-          GST_MESSAGE_STREAM_STATUS | GST_MESSAGE_ASYNC_DONE |
-          GST_MESSAGE_STREAM_START | GST_MESSAGE_TAG, GST_MESSAGE_EOS), NULL);
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  GstStateChangeReturn ret = gst_element_get_state (pipeline, NULL, NULL,
+      GST_CLOCK_TIME_NONE);
+
+  fail_unless (ret == GST_STATE_CHANGE_SUCCESS,
+      "Couldn't set pipeline to PLAYING: %s",
+      gst_element_state_change_return_get_name (ret));
+
+  GstBus *bus = gst_element_get_bus (pipeline);
+  while (1) {
+    GstMessage *message = gst_bus_poll (bus, GST_MESSAGE_ANY, GST_SECOND / 2);
+    GstMessageType rmessage;
+
+    if (message) {
+      rmessage = GST_MESSAGE_TYPE (message);
+      gst_message_unref (message);
+    } else {
+      rmessage = GST_MESSAGE_UNKNOWN;
+    }
+
+    if (rmessage == tmessage) {
+      break;
+    } else if (rmessage == GST_MESSAGE_UNKNOWN) {
+      fail ("Unexpected timeout in gst_bus_poll, looking for %d", tmessage);
+      break;
+    } else if (rmessage & message_types) {
+      continue;
+    }
+    fail ("Unexpected message received of type %d, '%s', looking for %d",
+        rmessage, gst_message_type_get_name (rmessage), tmessage);
+  }
 
   GST_INFO ("-- cleanup --");
-  gst_object_unref (pipeline);
-  g_free (str);
-  BT_TEST_END;
-}
-
-static void
-test_launch_bt_dec_push (BT_TEST_ARGS)
-{
-  BT_TEST_START;
-  GST_INFO ("-- arrange --");
-  gchar *str =
-      g_strdup_printf
-      ("filesrc location=%s ! queue ! buzztrax-dec ! fakesink sync=false",
-      check_get_test_song_path ("simple2.xml"));
-  GstElement *pipeline = gst_parse_launch (str, NULL);
-
-  GST_INFO ("-- act && assert --");
-  fail_unless (run_pipeline (pipeline,
-          GST_MESSAGE_NEW_CLOCK | GST_MESSAGE_STATE_CHANGED |
-          GST_MESSAGE_STREAM_STATUS | GST_MESSAGE_ASYNC_DONE |
-          GST_MESSAGE_STREAM_START | GST_MESSAGE_TAG, GST_MESSAGE_EOS), NULL);
-
-  GST_INFO ("-- cleanup --");
-  gst_object_unref (pipeline);
-  g_free (str);
-  BT_TEST_END;
-}
-
-static void
-test_launch_bt_dec_playbin (BT_TEST_ARGS)
-{
-  BT_TEST_START;
-  GST_INFO ("-- arrange --");
-  gchar *str =
-      g_strdup_printf
-      ("playbin uri=file://%s audio-sink=\"fakesink sync=false\"",
-      check_get_test_song_path ("simple2.xml"));
-  GstElement *pipeline = gst_parse_launch (str, NULL);
-
-  GST_INFO ("-- act && assert --");
-  fail_unless (run_pipeline (pipeline,
-          GST_MESSAGE_NEW_CLOCK | GST_MESSAGE_STATE_CHANGED |
-          GST_MESSAGE_STREAM_STATUS | GST_MESSAGE_ASYNC_DONE |
-          GST_MESSAGE_STREAM_START | GST_MESSAGE_TAG, GST_MESSAGE_EOS), NULL);
-
-  GST_INFO ("-- cleanup --");
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
   gst_object_unref (pipeline);
   g_free (str);
   BT_TEST_END;
@@ -170,9 +162,7 @@ gst_buzztrax_elements_example_case (void)
 {
   TCase *tc = tcase_create ("GstElementExamples");
 
-  tcase_add_test (tc, test_launch_bt_dec_pull);
-  tcase_add_test (tc, test_launch_bt_dec_push);
-  tcase_add_test (tc, test_launch_bt_dec_playbin);
+  tcase_add_loop_test (tc, test_launch_bt_dec, 0, G_N_ELEMENTS (pipeline_desc));
   tcase_add_unchecked_fixture (tc, case_setup, case_teardown);
   return tc;
 }
