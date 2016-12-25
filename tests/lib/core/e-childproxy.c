cfc69be68ee32662e8873111ad366fe293a82bcb
Stefan Sauer <ensonic@users.sf.net>
2016-12-25 19:56:06 +0100

    childproxy: expand and fix the tests
    Don't rely on the object defaults, but set defaults we test against. Add
    tests for the _property variants.

--- tests/lib/core/e-childproxy.c
+++ tests/lib/core/e-childproxy.c
@@ -20,6 +20,7 @@
 //-- globals
 
 static BtApplication *app;
+static BtSong *song;
 
 //-- fixtures
 
@@ -33,11 +34,15 @@ static void
 test_setup (void)
 {
   app = bt_test_application_new ();
+  song = bt_song_new (app);
+  bt_child_proxy_set (song, "sequence::length", 16, "sequence::loop", FALSE,
+      NULL);
 }
 
 static void
 test_teardown (void)
 {
+  ck_g_object_final_unref (song);
   ck_g_object_final_unref (app);
 }
 
@@ -57,7 +62,7 @@ test_bt_child_proxy_get (BT_TEST_ARGS)
   gulong length;
 
   GST_INFO ("-- act --");
-  bt_child_proxy_get (app, "song::sequence::length", &length, NULL);
+  bt_child_proxy_get (song, "sequence::length", &length, NULL);
 
   GST_INFO ("-- assert --");
   ck_assert_int_gt (length, 0);
@@ -71,17 +76,58 @@ test_bt_child_proxy_set (BT_TEST_ARGS)
 {
   BT_TEST_START;
   GST_INFO ("-- arrange --");
-  gboolean loop1, loop2;
-  bt_child_proxy_get (app, "song::sequence::loop", &loop1, NULL);
+  gboolean loop;
 
   GST_INFO ("-- act --");
-  bt_child_proxy_set (app, "song::sequence::loop", !loop1, NULL);
+  bt_child_proxy_set (song, "sequence::loop", TRUE, NULL);
 
   GST_INFO ("-- assert --");
-  bt_child_proxy_get (app, "song::sequence::loop", &loop2, NULL);
-  ck_assert_int_ne (loop1, loop2);
+  bt_child_proxy_get (song, "sequence::loop", &loop, NULL);
+  ck_assert_int_eq (TRUE, loop);
 
   GST_INFO ("-- cleanup --");
+  BT_TEST_END;
+}
+
+static void
+test_bt_child_proxy_get_property (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  GST_INFO ("-- arrange --");
+  GValue value = { 0, };
+  g_value_init (&value, G_TYPE_ULONG);
+
+  GST_INFO ("-- act --");
+  bt_child_proxy_get_property ((GObject *) song, "sequence::length", &value);
+  gulong length = g_value_get_ulong (&value);
+
+  GST_INFO ("-- assert --");
+  ck_assert_int_gt (length, 0);
+
+  GST_INFO ("-- cleanup --");
+  g_value_unset (&value);
+  BT_TEST_END;
+}
+
+static void
+test_bt_child_proxy_set_property (BT_TEST_ARGS)
+{
+  BT_TEST_START;
+  GST_INFO ("-- arrange --");
+  gboolean loop;
+  GValue value = { 0, };
+  g_value_init (&value, G_TYPE_BOOLEAN);
+  g_value_set_boolean (&value, TRUE);
+
+  GST_INFO ("-- act --");
+  bt_child_proxy_set_property ((GObject *) song, "sequence::loop", &value);
+
+  GST_INFO ("-- assert --");
+  bt_child_proxy_get (song, "sequence::loop", &loop, NULL);
+  ck_assert_int_eq (TRUE, loop);
+
+  GST_INFO ("-- cleanup --");
+  g_value_unset (&value);
   BT_TEST_END;
 }
 
@@ -92,6 +138,8 @@ bt_child_proxy_example_case (void)
 
   tcase_add_test (tc, test_bt_child_proxy_get);
   tcase_add_test (tc, test_bt_child_proxy_set);
+  tcase_add_test (tc, test_bt_child_proxy_get_property);
+  tcase_add_test (tc, test_bt_child_proxy_set_property);
   tcase_add_checked_fixture (tc, test_setup, test_teardown);
   tcase_add_unchecked_fixture (tc, case_setup, case_teardown);
   return tc;
