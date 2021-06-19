fbba2df8b219f008a23dcbf68dac7c9078861cac
David Beswick <dlbeswick@gmail.com>
2021-06-19 23:39:35 +1000

    Crude custom machine graphics

---
+++ src/lib/gst/ui.c
@@ -0,0 +1,67 @@
+/* Buzztrax
+ * Copyright (C) 2022 David Beswick <dlbeswick@gmail.com>
+ *
+ * lib/gst/ui.c: relating to UI functions of plugin machines.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+/**
+ * SECTION:ui
+ * @title: GstBtUiCustomGfxInterface
+ * @include: libtbuzztrax-gst/ui.h
+ * @short_description: custom gfx in machine view
+ *
+ * Allows a machine to supply its own graphics to be drawn over the machine in
+ * the Machine View.
+ *
+ * SIGNALS:
+ *
+ * gstbt-ui-gfx-invalidated() [G_SIGNAL_ACTION]
+ * 
+ * Machines generally don't have to do anything in response to this signal, but
+ * they should emit it when something happens that should change the image,
+ * i.e. by the machine on a parameter change.
+ */
+
+#include "ui.h"
+
+G_DEFINE_INTERFACE(GstBtUiCustomGfx, gstbt_ui_custom_gfx, G_TYPE_OBJECT)
+
+static void gstbt_ui_custom_gfx_default_init (GstBtUiCustomGfxInterface *iface) {
+  g_signal_new (
+    "gstbt-ui-custom-gfx-invalidated",
+    G_OBJECT_CLASS_TYPE(iface),
+    G_SIGNAL_ACTION | G_SIGNAL_RUN_LAST,
+    0 /* offset */,
+    NULL /* accumulator */,
+    NULL /* accumulator data */,
+    NULL /* C marshaller */,
+    G_TYPE_NONE /* return_type */,
+    0     /* n_params */);
+}
+
+const GstBtUiCustomGfxResponse* gstbt_ui_custom_gfx_request (GstBtUiCustomGfx *self) {
+  GstBtUiCustomGfxInterface *iface;
+
+  g_return_val_if_fail (GSTBT_UI_IS_CUSTOM_GFX (self), 0);
+
+  iface = GSTBT_UI_CUSTOM_GFX_GET_IFACE (self);
+  g_return_val_if_fail (iface->request != NULL, 0);
+  return iface->request (self);
+}
---
+++ src/lib/gst/ui.h
@@ -0,0 +1,68 @@
+/* Buzztrax
+ * Copyright (C) 2022 David Beswick <dlbeswick@gmail.com>
+ *
+ * lib/gst/ui.h: relating to UI functions of plugin machines.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIB_GST_UI_H__
+#define __LIB_GST_UI_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/**
+ * GstBtUiCustomGfxResponse: returned by GstBtUiCustomGfxInterface::request
+ *
+ * @version: set to 0
+ * @width: width of returned image in pixels
+ * @height: height of returned image in pixels
+ * @data: ABGR data, width*height guints
+ */
+typedef struct GstBtUiCustomGfxResponse {
+  guint version; // set to 0
+  guint width;
+  guint height;
+  guint32* data;   // ABGR data, width*height guints
+} GstBtUiCustomGfxResponse;
+
+
+#define GSTBT_UI_TYPE_CUSTOM_GFX gstbt_ui_custom_gfx_get_type ()
+G_DECLARE_INTERFACE (GstBtUiCustomGfx, gstbt_ui_custom_gfx, GSTBT_UI, CUSTOM_GFX,
+                     GObject)
+
+/**
+ * GstBtUiCustomGfxInterface
+ *
+ * @request: Return the machine's current custom gfx image.
+ *           Ownership of the returned object isn't transferred, so cached data
+ *           can be used (both response structure and image data.)
+ *
+ *           The actual rendering of the image may be done here.
+ *
+ *           May return NULL; in that case, Buzztrax's standard gfx are used.
+*/
+struct _GstBtUiCustomGfxInterface {
+  GTypeInterface parent;
+
+  const GstBtUiCustomGfxResponse *(*request) (GstBtUiCustomGfx *self);
+};
+
+const GstBtUiCustomGfxResponse *gstbt_ui_custom_gfx_request (GstBtUiCustomGfx *self);
+
+G_END_DECLS
+
+#endif
